41b14f29454a4a24adc50f4e5eb7a30d7ffa4dac
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-08-05 10:45:34 -0500

    sbatch

--- Ours/main3.py
+++ Ours/main3.py
@@ -6,29 +6,31 @@ sys.path.append('/scratch/midway3/ilgee/SelfGCon')
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import pandas as pd
 
 from torch_geometric.datasets import Planetoid, Coauthor, Amazon
 import torch_geometric.transforms as T
 
 from model import * ############## model 2
 from aug import *
-from cluster import *
+# from cluster import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='SelfGCon')
-parser.add_argument('--dataset', type=str, default='Cora')
+parser.add_argument('--dataset', type=str, default='CiteSeer')
 parser.add_argument('--split', type=str, default='PublicSplit')
-parser.add_argument('--epochs', type=int, default=50)
+parser.add_argument('--epochs', type=int, default=20)
 parser.add_argument('--n_experiments', type=int, default=1)
-parser.add_argument('--n_layers', type=int, default=2) #CiteSeer: 1, Rest: 2
+parser.add_argument('--n_layers', type=int, default=1) #CiteSeer: 1, Rest: 2
 parser.add_argument('--channels', type=int, default=512) #512
 parser.add_argument('--tau', type=float, default=0.5) #
 parser.add_argument('--lr1', type=float, default=1e-3) #
 parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd1', type=float, default=0.0)
-parser.add_argument('--wd2', type=float, default=1e-4)
-parser.add_argument('--edr', type=float, default=0.2)
+parser.add_argument('--wd2', type=float, default=1e-2)
+parser.add_argument('--edr', type=float, default=0.1)
 parser.add_argument('--fmr', type=float, default=0.4)
+parser.add_argument('--result_file', type=str) #, default="/results/SelfGCon_node_classification"
 args = parser.parse_args()
 
 file_path = os.getcwd() + args.result_file
@@ -143,3 +145,6 @@ for exp in range(args.n_experiments):
 
         print('Epoch:{}, train_acc:{:.4f}, val_acc:{:4f}, test_acc:{:4f}'.format(epoch, train_acc, val_acc, test_acc))
         print('Linear evaluation accuracy:{:.4f}'.format(eval_acc))
+    results += [[args.model, args.dataset, args.epochs, args.n_layers, args.tau, args.lr1, args.lr2, args.wd2, args.channels, args.edr, args.fmr, eval_acc]]
+    res1 = pd.DataFrame(results, columns=['model', 'dataset', 'epochs', 'layers', 'tau', 'lr1', 'lr2', 'wd2', 'channels', 'edge_drop_rate', 'feat_mask_rate', 'accuracy'])
+    res1.to_csv(file_path + "_" + args.dataset +  ".csv", index=False)
