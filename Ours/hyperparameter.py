700caa0108d442b428182631d3e60d012af75991
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-08-09 11:12:03 -0500

    new_sbatch_file

--- Ours/hyperparameter.py
+++ Ours/hyperparameter.py
@@ -1,14 +1,13 @@
 import sys, os
 import os.path as osp
-# sys.path.append('/scratch/midway3/ilgee/SelfGCon')
 import argparse
-import pdb
-
+sys.path.append('/scratch/midway3/ilgee/SelfGCon')
+# import pdb
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import wandb
-import yaml
+# import yaml
 
 from torch_geometric.datasets import Planetoid, Coauthor, Amazon
 import torch_geometric.transforms as T
@@ -17,7 +16,7 @@ from torch_geometric.utils import add_self_loops
 
 from model import *
 from aug import *
-from cluster import *
+# from cluster import *
 
 os.environ['JOBLIB_TEMP_FOLDER'] = '/tmp'
 # with open(r'config/config.yaml') as file:
@@ -61,26 +60,29 @@ parameters = {
         'values' : [1e-4] #, 1e-3, 1e-2
     },
     'edr': {
-        'values' : [0.2,0.3,0.4,0.5] # (Cora) 0,0.1,
+        'values' : [0.2] # (Cora) 0,0.1, ,0.3,0.4,0.5
     },
     'fmr': {
-        'values' : [0.2,0.3,0.4,0.5] # (Cora) 0,0.1,
+        'values' : [0.2] # (Cora) 0,0.1,,0.3,0.4,0.5
     },
+    'use_mlp': {
+        'values' : [False]
+    }
 }         
 sweep_config['metric'] = metric
 sweep_config['parameters'] = parameters
 
-sweep_id = wandb.sweep(sweep_config, project="PubMed_hyperparameter_tunning") ##########################
+sweep_id = wandb.sweep(sweep_config, project="SelfGCon_Cora_hyperparameter_tunning")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def main(args=None):    
     with wandb.init(config=sweep_config):
         args = wandb.config
         def train(model, data):
-            edr = args.edr
-            fmr = args.fmr
-            new_data1 = random_aug(data, edr, fmr)
-            new_data2 = random_aug(data, edr, fmr)
+            model.train()
+            optimizer.zero_grad()
+            new_data1 = random_aug(data, args.edr, args.fmr)
+            new_data2 = random_aug(data, args.edr, args.fmr)
             new_data1 = new_data1.to(device)
             new_data2 = new_data2.to(device)
             z1, z2 = model(new_data1, new_data2)   
@@ -89,21 +91,18 @@ def main(args=None):
             optimizer.step()
             return loss.item()
 
-        results =[]
+        # results =[]
         for exp in range(args.n_experiments): 
             if args.split == "PublicSplit":
-                transform = T.Compose([T.NormalizeFeatures()]) #, T.RandomNodeSplit(split="random", 
+                transform = T.Compose([T.NormalizeFeatures(),T.ToDevice(device)]) #, T.RandomNodeSplit(split="random", 
                                                                                 #                   num_train_per_class = 20,
                                                                                 #                   num_val = 500,
                                                                                 #                   num_test = 1000)])
-                                                                                # ,T.ToDevice(device)
             if args.split == "RandomSplit":
-                transform = T.Compose([T.NormalizeFeatures(), T.RandomNodeSplit(split="random", 
+                transform = T.Compose([T.NormalizeFeatures(),T.ToDevice(device), T.RandomNodeSplit(split="random", 
                                                                                                     num_train_per_class = 20,
                                                                                                     num_val = 160,
                                                                                                     num_test = 1280)])
-                                                                                                    # ,T.ToDevice(device)
-
             if args.dataset in ['Cora', 'CiteSeer', 'PubMed']:
                 dataset = Planetoid(root='Planetoid', name=args.dataset, transform=transform)
                 data = dataset[0]
@@ -117,28 +116,23 @@ def main(args=None):
             train_idx = data.train_mask 
             val_idx = data.val_mask 
             test_idx = data.test_mask  
-
             in_dim = data.num_features
             hid_dim = args.channels
             out_dim = args.channels
             n_layers = args.n_layers
             tau = args.tau
-
-            dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
             num_class = int(data.y.max().item()) + 1 
             N = data.num_nodes
 
-            ##### Train the SelfGCon model #####
-            print("=== train SelfGCon model ===")
-            model = SelfGCon(in_dim, hid_dim, out_dim, n_layers, tau, use_mlp=False)
+            ##### Train model #####
+            print("=== train model ===")
+            model = SelfGCon(in_dim, hid_dim, out_dim, n_layers, tau, use_mlp=args.use_mlp)
+            model = model.to(device)
             optimizer = torch.optim.Adam(model.parameters(), lr=args.lr1, weight_decay=args.wd1)
             for epoch in range(args.epochs):
-                model.train()
-                optimizer.zero_grad()
                 loss = train(model, data)
-                print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss))
+                # print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss))
             
-            data = data.to(device)
             embeds = model.get_embedding(data)
 
             train_embs = embeds[train_idx]
@@ -154,6 +148,7 @@ def main(args=None):
 
             ''' Linear Evaluation '''
             logreg = LogReg(train_embs.shape[1], num_class)
+            logreg = logreg.to(device)
             opt = torch.optim.Adam(logreg.parameters(), lr=args.lr2, weight_decay=args.wd2)
             loss_fn = nn.CrossEntropyLoss()
 
