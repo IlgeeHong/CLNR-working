9744ba59f8103189c32cf71026e5cb5c780cc79b
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-08-09 09:41:19 -0500

    tunning

--- Ours/model2.py
+++ Ours/model2.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
+###############################
+# This is for scalable method #
+###############################
 
 import torch
 import torch.nn as nn
