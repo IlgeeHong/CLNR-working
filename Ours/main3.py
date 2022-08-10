386a1e96a8cbcb37ff857c08a5c2217d89263139
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-08-10 13:21:53 -0500

    computers

--- Ours/main3.py
+++ Ours/main3.py
@@ -17,19 +17,19 @@ from aug import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='SelfGCon')
-parser.add_argument('--dataset', type=str, default='CiteSeer')
-parser.add_argument('--split', type=str, default='PublicSplit')
+parser.add_argument('--dataset', type=str, default='Computers')
+parser.add_argument('--split', type=str, default='RandomSplit')
 parser.add_argument('--epochs', type=int, default=50)
-parser.add_argument('--n_experiments', type=int, default=20)
-parser.add_argument('--n_layers', type=int, default=1) 
+parser.add_argument('--n_experiments', type=int, default=1)
+parser.add_argument('--n_layers', type=int, default=2) 
 parser.add_argument('--channels', type=int, default=512) 
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr1', type=float, default=1e-3) 
 parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd1', type=float, default=0.0)
 parser.add_argument('--wd2', type=float, default=1e-4)
-parser.add_argument('--edr', type=float, default=0.4)
-parser.add_argument('--fmr', type=float, default=0.0)
+parser.add_argument('--edr', type=float, default=0.1)
+parser.add_argument('--fmr', type=float, default=0.3)
 parser.add_argument('--result_file', type=str, default="/Ours/results/Final_accuracy")
 args = parser.parse_args()
 
@@ -57,10 +57,9 @@ for exp in range(args.n_experiments):
                                                                          #                   num_val = 500,
                                                                          #                   num_test = 1000)])
     if args.split == "RandomSplit":
-        transform = T.Compose([T.NormalizeFeatures(),T.ToDevice(device), T.RandomNodeSplit(split="random", 
-                                                                                            num_train_per_class = 20,
-                                                                                            num_val = 160,
-                                                                                            num_test = 1280)])
+        transform = T.Compose([T.NormalizeFeatures(),T.ToDevice(device), T.RandomNodeSplit(split="train_rest", 
+                                                                                            num_val = 0.1,
+                                                                                            num_test = 0.8)])                                                                                        num_test = 1280)])
 
     if args.dataset in ['Cora', 'CiteSeer', 'PubMed']:
         dataset = Planetoid(root='Planetoid', name=args.dataset, transform=transform)
@@ -69,7 +68,7 @@ for exp in range(args.n_experiments):
         dataset = Coauthor(args.dataset, 'public', transform=transform)
         data = dataset[0]
     if args.dataset in ['Computers', 'Photo']:
-        dataset = Amazon("/Users/ilgeehong/Desktop/SemGCon/", args.dataset, transform=transform)
+        dataset = Amazon("/scratch/midway3/ilgee/SelfGCon", args.dataset, transform=transform)
         data = dataset[0]
 
     train_idx = data.train_mask 
