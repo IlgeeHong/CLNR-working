5f620e349c757f259bc24ff8b46d6cbcaf6c8e5a
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-08-10 11:25:11 -0500

    -

--- Ours/hyperparameter_citeseer.py
+++ Ours/hyperparameter_citeseer.py
@@ -19,11 +19,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='SelfGCon')
 parser.add_argument('--dataset', type=str, default='CiteSeer')
 parser.add_argument('--split', type=str, default='PublicSplit')
-parser.add_argument('--lr1', type=float, default=1e-3) #
+parser.add_argument('--lr1', type=float, default=1e-3) 
 parser.add_argument('--wd1', type=float, default=0.0)
 parser.add_argument('--mlp_use', type=bool, default=False)
 parser.add_argument('--result_file', type=str, default="/Ours/results/Slurm")
-parser.add_argument('--n_experiments', type=int, default=10)
+parser.add_argument('--n_experiments', type=int, default=3)
 parser.add_argument('--tau', type=float, default=0.5)
 args = parser.parse_args()
 
@@ -43,43 +43,43 @@ def train(model, data, edr, fmr):
     optimizer.step()
     return loss.item()
 
-
-if args.split == "PublicSplit":
-    transform = T.Compose([T.NormalizeFeatures(),T.ToDevice(device)]) 
-if args.split == "RandomSplit":
-    transform = T.Compose([T.NormalizeFeatures(),T.ToDevice(device), T.RandomNodeSplit(split="random", 
-                                                                                        num_train_per_class = 20,
-                                                                                        num_val = 160,
-                                                                                        num_test = 1280)])
-if args.dataset in ['Cora', 'CiteSeer', 'PubMed']:
-    dataset = Planetoid(root='Planetoid', name=args.dataset, transform=transform)
-    data = dataset[0]
-if args.dataset in ['cs', 'physics']:
-    dataset = Coauthor(args.dataset, 'public', transform=transform)
-    data = dataset[0]
-if args.dataset in ['Computers', 'Photo']:
-    dataset = Amazon("/Users/ilgeehong/Desktop/SemGCon/", args.dataset, transform=transform)
-    data = dataset[0]
-
-train_idx = data.train_mask 
-val_idx = data.val_mask 
-test_idx = data.test_mask  
-in_dim = data.num_features
-tau = args.tau
-num_class = int(data.y.max().item()) + 1 
-N = data.num_nodes
 results =[]
-for channels in [256, 512]: #
-    hid_dim = channels
-    out_dim = channels
-    for n_layers in [1, 2]: #
-        for edr in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]: #
-            for fmr in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]: #
-                for lr2 in [1e-2, 5e-3]: #
-                    for wd2 in [1e-2, 1e-4]: #
-                        for epochs in [20, 50, 100]: #
+for channels in [256, 512]:
+    for n_layers in [1, 2]: 
+        for edr in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]:
+            for fmr in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]: 
+                for lr2 in [1e-2, 5e-3]:  
+                    for wd2 in [1e-2, 1e-4]:
+                        for epochs in [20, 50, 100]:
                             best_val_acc_list = []
                             for exp in range(args.n_experiments): 
+                                if args.split == "PublicSplit":
+                                    transform = T.Compose([T.NormalizeFeatures(),T.ToDevice(device)]) 
+                                if args.split == "RandomSplit":
+                                    transform = T.Compose([T.NormalizeFeatures(),T.ToDevice(device), T.RandomNodeSplit(split="random", 
+                                                                                                                        num_train_per_class = 20,
+                                                                                                                        num_val = 160,
+                                                                                                                        num_test = 1280)])
+                                if args.dataset in ['Cora', 'CiteSeer', 'PubMed']:
+                                    dataset = Planetoid(root='Planetoid', name=args.dataset, transform=transform)
+                                    data = dataset[0]
+                                if args.dataset in ['cs', 'physics']:
+                                    dataset = Coauthor(args.dataset, 'public', transform=transform)
+                                    data = dataset[0]
+                                if args.dataset in ['Computers', 'Photo']:
+                                    dataset = Amazon("/Users/ilgeehong/Desktop/SemGCon/", args.dataset, transform=transform)
+                                    data = dataset[0]
+
+                                train_idx = data.train_mask 
+                                val_idx = data.val_mask 
+                                test_idx = data.test_mask  
+                                in_dim = data.num_features
+                                hid_dim = channels
+                                out_dim = channels
+                                tau = args.tau
+                                num_class = int(data.y.max().item()) + 1 
+                                N = data.num_nodes
+
                                 ''' Model Pretraining '''
                                 model = SelfGCon(in_dim, hid_dim, out_dim, n_layers, tau, use_mlp=args.mlp_use)
                                 model = model.to(device)
@@ -123,6 +123,7 @@ for channels in [256, 512]: #
                                             best_val_acc = val_acc
                                             if test_acc > eval_acc:
                                                 eval_acc = test_acc
+                                # print('Linear evaluation accuracy:{:.4f}'.format(best_val_acc))                            
                                 best_val_acc_list.append(best_val_acc.item())
                             best_val_acc_mean = mean(best_val_acc_list)
                             results += [[args.model, args.dataset, epochs, n_layers, args.tau, args.lr1, args.wd1, lr2, wd2, channels, edr, fmr, args.mlp_use, best_val_acc_mean]]
