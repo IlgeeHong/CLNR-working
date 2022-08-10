66a5b177bc12f0126e769238dfdb7981c6961201
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-08-09 20:09:11 -0500

    main3_edited

--- Ours/results/result.R
+++ Ours/results/result.R
@@ -1,12 +1,18 @@
 setwd("/Users/ilgeehong/Desktop/SemGCon/Ours/results")
-selfgcon_cora = read.csv("SelfGCon_node_classification_Cora.csv")
-mean(as.numeric(substr(selfgcon_cora$accuracy,8,13)))
-sd(as.numeric(substr(selfgcon_cora$accuracy,8,13)))
+#selfgcon_cora = read.csv("SelfGCon_node_classification_Cora.csv")
+#mean(as.numeric(substr(selfgcon_cora$accuracy,8,13)))
+#sd(as.numeric(substr(selfgcon_cora$accuracy,8,13)))
 
-selfgcon_citeseer = read.csv("SelfGCon_node_classification_CiteSeer.csv")
-mean(as.numeric(substr(selfgcon_citeseer$accuracy,8,13)))
-sd(as.numeric(substr(selfgcon_citeseer$accuracy,8,13)))
+#selfgcon_citeseer = read.csv("SelfGCon_node_classification_CiteSeer.csv")
+#mean(as.numeric(substr(selfgcon_citeseer$accuracy,8,13)))
+#sd(as.numeric(substr(selfgcon_citeseer$accuracy,8,13)))
 
-selfgcon_pubmed = read.csv("SelfGCon_node_classification_PubMed.csv")
-mean(as.numeric(substr(selfgcon_pubmed$accuracy,8,13)))
-sd(as.numeric(substr(selfgcon_pubmed$accuracy,8,13)))
+#selfgcon_pubmed = read.csv("SelfGCon_node_classification_PubMed.csv")
+#mean(as.numeric(substr(selfgcon_pubmed$accuracy,8,13)))
+#sd(as.numeric(substr(selfgcon_pubmed$accuracy,8,13)))
+
+cora = read.csv("Slurm_SelfGCon_Cora.csv")
+cora[order(cora$val_acc, decreasing = TRUE),]
+
+citeseer = read.csv("Slurm_SelfGCon_CiteSeer.csv")
+citeseer[order(citeseer$val_acc, decreasing = TRUE),]
