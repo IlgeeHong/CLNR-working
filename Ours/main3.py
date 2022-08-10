feee2dba5a326dcd530b124ffdf0a5dffff2c43d
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-08-10 13:58:35 -0500

    -

--- Ours/main3.py
+++ Ours/main3.py
@@ -18,7 +18,7 @@ from aug import *
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='SelfGCon')
 parser.add_argument('--dataset', type=str, default='Computers')
-parser.add_argument('--split', type=str, default='RamdomSplit')
+parser.add_argument('--split', type=str, default='RandomSplit')
 parser.add_argument('--epochs', type=int, default=50)
 parser.add_argument('--n_experiments', type=int, default=20)
 parser.add_argument('--n_layers', type=int, default=2) 
