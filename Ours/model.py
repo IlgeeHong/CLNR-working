c7cc402680ecf9c27e9547d91a2df72ae9f29e2f
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-08-18 11:52:08 -0500

    -

--- Ours/model.py
+++ Ours/model.py
@@ -104,3 +104,44 @@ class SelfGCon(nn.Module):
         ret = (l1 + l2) * 0.5
         ret = ret.mean() if mean else ret.sum()
         return ret
+
+class SemiGCon(nn.Module):
+    def __init__(self, in_dim, hid_dim, out_dim, n_layers, tau = 0.5, use_mlp = False):
+        super().__init__()
+        if not use_mlp:
+            self.backbone = GCN(in_dim, hid_dim, out_dim, n_layers)
+        else:
+            self.backbone = MLP(in_dim, hid_dim, out_dim)
+        self.tau = tau
+
+    def get_embedding(self, data):
+        out = self.backbone(data.x, data.edge_index)
+        return out.detach()
+
+    def forward(self, data1, data2):
+        h1 = self.backbone(data1.x, data1.edge_index)
+        h2 = self.backbone(data2.x, data2.edge_index)
+        z1 = (h1 - h1.mean(0)) / h1.std(0)
+        z2 = (h2 - h2.mean(0)) / h2.std(0)
+        return z1, z2
+    
+    def sim(self, z1, z2):
+        z1 = F.normalize(z1)
+        z2 = F.normalize(z2)
+        return torch.mm(z1, z2.t())
+
+    def semi_loss(self, z1, z2, num_per_class, pos_idx):
+        f = lambda x: torch.exp(x / self.tau) 
+        refl_sim = f(self.sim(z1, z1))
+        between_sim = f(self.sim(z1, z2))
+        N = between_sim.shape[1]
+        return -torch.log(
+            (1/(2*num_per_class-1))*(between_sim[pos_idx].reshape(N,num_per_class).sum(1) + refl_sim[pos_idx].reshape(N,num_per_class).sum(1) - refl_sim.diag())
+            / (between_sim.sum(1) + refl_sim.sum(1) - refl_sim.diag()))
+
+    def loss(self, z1, z2, num_per_class, pos_idx, mean = True):
+        l1 = self.semi_loss(z1, z2, num_per_class, pos_idx)
+        l2 = self.semi_loss(z2, z1, num_per_class, pos_idx)
+        ret = (l1 + l2) * 0.5
+        ret = ret.mean() if mean else ret.sum()
+        return ret
