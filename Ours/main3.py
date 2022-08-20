f64b31bcd51843de18b18568b4a344f3a62ac5c8
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-08-20 12:04:43 -0500

    -

--- Ours/main3.py
+++ Ours/main3.py
@@ -16,20 +16,20 @@ from aug import *
 # from cluster import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default='SemiGCon')
-parser.add_argument('--dataset', type=str, default='PubMed')
+parser.add_argument('--model', type=str, default='SelfGCon')
+parser.add_argument('--dataset', type=str, default='CiteSeer')
 parser.add_argument('--split', type=str, default='PublicSplit')
 parser.add_argument('--epochs', type=int, default=50)
 parser.add_argument('--n_experiments', type=int, default=20)
-parser.add_argument('--n_layers', type=int, default=2) 
-parser.add_argument('--channels', type=int, default=512) 
+parser.add_argument('--n_layers', type=int, default=1) 
+parser.add_argument('--channels', type=int, default=256) 
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr1', type=float, default=1e-3) 
 parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd1', type=float, default=0.0)
 parser.add_argument('--wd2', type=float, default=1e-2)
-parser.add_argument('--edr', type=float, default=0.0)
-parser.add_argument('--fmr', type=float, default=0.0)
+parser.add_argument('--edr', type=float, default=0.5)
+parser.add_argument('--fmr', type=float, default=0.2)
 parser.add_argument('--result_file', type=str, default="/Ours/results/Final_accuracy")
 args = parser.parse_args()
 
@@ -104,11 +104,11 @@ for exp in range(args.n_experiments):
 
     ##### Train the SelfGCon model #####
     print("=== train SelfGCon model ===")
-    model = SemiGCon(in_dim, hid_dim, out_dim, n_layers, tau, use_mlp=False)
+    model = SelfGCon(in_dim, hid_dim, out_dim, n_layers, tau, use_mlp=False) #
     model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr1, weight_decay=0)
     for epoch in range(args.epochs):
-        loss = train_semi(model, data, num_per_class, pos_idx)
+        loss = train(model, data) # train_semi(model, data, num_per_class, pos_idx) #
         # print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss))
     
     embeds = model.get_embedding(data)
