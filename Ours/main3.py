e2de191d626d272f20323329bc7ef68142e10869
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-08-31 12:16:10 -0500

    -

--- Ours/main3.py
+++ Ours/main3.py
@@ -17,7 +17,7 @@ from aug import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='SelfGCon') #SemiGCon
-parser.add_argument('--dataset', type=str, default='CS')
+parser.add_argument('--dataset', type=str, default='Photo')
 parser.add_argument('--split', type=str, default='RandomSplit') #PublicSplit
 parser.add_argument('--epochs', type=int, default=50)
 parser.add_argument('--n_experiments', type=int, default=20)
@@ -27,9 +27,9 @@ parser.add_argument('--tau', type=float, default=0.5)
 parser.add_argument('--lr1', type=float, default=1e-3) 
 parser.add_argument('--lr2', type=float, default=5e-3)
 parser.add_argument('--wd1', type=float, default=0.0)
-parser.add_argument('--wd2', type=float, default=1e-2)
-parser.add_argument('--edr', type=float, default=0.1)
-parser.add_argument('--fmr', type=float, default=0.3)
+parser.add_argument('--wd2', type=float, default=1e-4)
+parser.add_argument('--edr', type=float, default=0.5)
+parser.add_argument('--fmr', type=float, default=0.1)
 parser.add_argument('--mlp_use', type=bool, default=True)
 parser.add_argument('--result_file', type=str, default="/Ours/hyperparameter/results/Final_accuracy")
 args = parser.parse_args()
