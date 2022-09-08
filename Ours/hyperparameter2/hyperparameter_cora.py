e17a6e524112f8870b58701e43449d53cce38212
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-09-08 15:57:11 -0500

    -

--- Ours/hyperparameter2/hyperparameter_cora.py
+++ Ours/hyperparameter2/hyperparameter_cora.py
@@ -21,11 +21,11 @@ parser.add_argument('--dataset', type=str, default='Cora')
 parser.add_argument('--split', type=str, default='PublicSplit')
 parser.add_argument('--lr1', type=float, default=1e-3) 
 parser.add_argument('--lr2', type=float, default=1e-2)
-parser.add_argument('--wd1', type=float, default=1e-5)
+# parser.add_argument('--wd1', type=float, default=1e-5)
 parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--tau', type=float, default=0.5)
 parser.add_argument('--mlp_use', type=bool, default=False)
-parser.add_argument('--result_file', type=str, default="/Ours/hyperparameter2/results/Hyperparameter")
+parser.add_argument('--result_file', type=str, default="/Ours/hyperparameter2/results/Hyperparameter2")
 parser.add_argument('--n_experiments', type=int, default=10)
 args = parser.parse_args()
 
@@ -50,79 +50,80 @@ for channels in [256, 512]:
     for n_layers in [2]: 
         for edr in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]:
             for fmr in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]: 
-                for epochs in [20, 50, 100]:
-                    best_val_acc_list = []
-                    for exp in range(args.n_experiments): 
-                        if args.split == "PublicSplit":
-                            transform = T.Compose([T.NormalizeFeatures(),T.ToDevice(device)]) 
-                        if args.split == "RandomSplit":
-                            transform = T.Compose([T.ToDevice(device),T.RandomNodeSplit(split="train_rest", num_val = 0.1, num_test = 0.8)])
-                        if args.dataset in ['Cora', 'CiteSeer', 'PubMed']:
-                            dataset = Planetoid(root='Planetoid', name=args.dataset, transform=transform)
-                            data = dataset[0]
-                        if args.dataset in ['CS', 'Physics']:
-                            dataset = Coauthor("/scratch/midway3/ilgee/SelfGCon", args.dataset, transform=transform)
-                            data = dataset[0]
-                        if args.dataset in ['Computers', 'Photo']:
-                            dataset = Amazon("/scratch/midway3/ilgee/SelfGCon", args.dataset, transform=transform)
-                            data = dataset[0]
+                for epochs in [20, 50]:
+                    for wd1 in [0.0, 1e-5]:
+                        best_val_acc_list = []
+                        for exp in range(args.n_experiments): 
+                            if args.split == "PublicSplit":
+                                transform = T.Compose([T.NormalizeFeatures(),T.ToDevice(device)]) 
+                            if args.split == "RandomSplit":
+                                transform = T.Compose([T.ToDevice(device),T.RandomNodeSplit(split="train_rest", num_val = 0.1, num_test = 0.8)])
+                            if args.dataset in ['Cora', 'CiteSeer', 'PubMed']:
+                                dataset = Planetoid(root='Planetoid', name=args.dataset, transform=transform)
+                                data = dataset[0]
+                            if args.dataset in ['CS', 'Physics']:
+                                dataset = Coauthor("/scratch/midway3/ilgee/SelfGCon", args.dataset, transform=transform)
+                                data = dataset[0]
+                            if args.dataset in ['Computers', 'Photo']:
+                                dataset = Amazon("/scratch/midway3/ilgee/SelfGCon", args.dataset, transform=transform)
+                                data = dataset[0]
 
-                        train_idx = data.train_mask 
-                        val_idx = data.val_mask 
-                        test_idx = data.test_mask  
-                        in_dim = data.num_features
-                        hid_dim = channels
-                        out_dim = channels
-                        num_class = int(data.y.max().item()) + 1 
-                        N = data.num_nodes
+                            train_idx = data.train_mask 
+                            val_idx = data.val_mask 
+                            test_idx = data.test_mask  
+                            in_dim = data.num_features
+                            hid_dim = channels
+                            out_dim = channels
+                            num_class = int(data.y.max().item()) + 1 
+                            N = data.num_nodes
 
-                        ''' Model Pretraining '''
-                        model = CLGR(in_dim, hid_dim, out_dim, n_layers, args.tau, use_mlp=args.mlp_use)
-                        model = model.to(device)
-                        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr1, weight_decay=args.wd1)                       
-                        for epoch in range(epochs):
-                            loss = train(model, data, fmr, edr)
-                        embeds = model.get_embedding(data)
-                        train_embs = embeds[train_idx]
-                        val_embs = embeds[val_idx]
-                        test_embs = embeds[test_idx]
-                        label = data.y
-                        label = label.to(device)
-                        train_labels = label[train_idx]
-                        val_labels = label[val_idx]
-                        test_labels = label[test_idx]
-                        
-                        ''' Linear Evaluation '''
-                        logreg = LogReg(train_embs.shape[1], num_class)
-                        logreg = logreg.to(device)
-                        opt = torch.optim.Adam(logreg.parameters(), lr=args.lr2, weight_decay=args.wd2)
-                        loss_fn = nn.CrossEntropyLoss()
-                        best_val_acc = 0
-                        eval_acc = 0
-                        for epoch in range(2000):
-                            logreg.train()
-                            opt.zero_grad()
-                            logits = logreg(train_embs)
-                            preds = torch.argmax(logits, dim=1)
-                            train_acc = torch.sum(preds == train_labels).float() / train_labels.shape[0]
-                            loss = loss_fn(logits, train_labels)
-                            loss.backward()
-                            opt.step()
-                            logreg.eval()
-                            with torch.no_grad():
-                                val_logits = logreg(val_embs)
-                                test_logits = logreg(test_embs)
-                                val_preds = torch.argmax(val_logits, dim=1)
-                                test_preds = torch.argmax(test_logits, dim=1)
-                                val_acc = torch.sum(val_preds == val_labels).float() / val_labels.shape[0]
-                                test_acc = torch.sum(test_preds == test_labels).float() / test_labels.shape[0]
-                                if val_acc >= best_val_acc:
-                                    best_val_acc = val_acc
-                                    if test_acc > eval_acc:
-                                        eval_acc = test_acc
-                        # print('Linear evaluation accuracy:{:.4f}'.format(best_val_acc))                            
-                        best_val_acc_list.append(best_val_acc.item())
-                    best_val_acc_mean = mean(best_val_acc_list)
-                    results += [[args.model, args.dataset, epochs, n_layers, args.tau, args.lr1, args.wd1, args.lr2, args.wd2, channels, fmr, edr, args.mlp_use, best_val_acc_mean]]
-                    res1 = pd.DataFrame(results, columns=['model', 'dataset', 'epochs', 'layers', 'tau', 'lr1', 'wd1', 'lr2', 'wd2', 'channels', 'feat_mask_rate', 'edge_drop_rate', 'mlp_use', 'val_acc'])
-                    res1.to_csv(file_path + "_" + args.model + "_" + args.dataset +  ".csv", index=False)
+                            ''' Model Pretraining '''
+                            model = CLGR(in_dim, hid_dim, out_dim, n_layers, args.tau, use_mlp=args.mlp_use)
+                            model = model.to(device)
+                            optimizer = torch.optim.Adam(model.parameters(), lr=args.lr1, weight_decay=wd1)                       
+                            for epoch in range(epochs):
+                                loss = train(model, data, fmr, edr)
+                            embeds = model.get_embedding(data)
+                            train_embs = embeds[train_idx]
+                            val_embs = embeds[val_idx]
+                            test_embs = embeds[test_idx]
+                            label = data.y
+                            label = label.to(device)
+                            train_labels = label[train_idx]
+                            val_labels = label[val_idx]
+                            test_labels = label[test_idx]
+                            
+                            ''' Linear Evaluation '''
+                            logreg = LogReg(train_embs.shape[1], num_class)
+                            logreg = logreg.to(device)
+                            opt = torch.optim.Adam(logreg.parameters(), lr=args.lr2, weight_decay=args.wd2)
+                            loss_fn = nn.CrossEntropyLoss()
+                            best_val_acc = 0
+                            eval_acc = 0
+                            for epoch in range(2000):
+                                logreg.train()
+                                opt.zero_grad()
+                                logits = logreg(train_embs)
+                                preds = torch.argmax(logits, dim=1)
+                                train_acc = torch.sum(preds == train_labels).float() / train_labels.shape[0]
+                                loss = loss_fn(logits, train_labels)
+                                loss.backward()
+                                opt.step()
+                                logreg.eval()
+                                with torch.no_grad():
+                                    val_logits = logreg(val_embs)
+                                    test_logits = logreg(test_embs)
+                                    val_preds = torch.argmax(val_logits, dim=1)
+                                    test_preds = torch.argmax(test_logits, dim=1)
+                                    val_acc = torch.sum(val_preds == val_labels).float() / val_labels.shape[0]
+                                    test_acc = torch.sum(test_preds == test_labels).float() / test_labels.shape[0]
+                                    if val_acc >= best_val_acc:
+                                        best_val_acc = val_acc
+                                        if test_acc > eval_acc:
+                                            eval_acc = test_acc
+                            # print('Linear evaluation accuracy:{:.4f}'.format(best_val_acc))                            
+                            best_val_acc_list.append(best_val_acc.item())
+                        best_val_acc_mean = mean(best_val_acc_list)
+                        results += [[args.model, args.dataset, epochs, n_layers, args.tau, args.lr1, wd1, args.lr2, args.wd2, channels, fmr, edr, args.mlp_use, best_val_acc_mean]]
+                        res1 = pd.DataFrame(results, columns=['model', 'dataset', 'epochs', 'layers', 'tau', 'lr1', 'wd1', 'lr2', 'wd2', 'channels', 'feat_mask_rate', 'edge_drop_rate', 'mlp_use', 'val_acc'])
+                        res1.to_csv(file_path + "_" + args.model + "_" + args.dataset +  ".csv", index=False)
