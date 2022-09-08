a2005880cd6e829c54d3a54ebdd46757b0ae5fde
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-09-08 15:23:18 -0500

    -

--- Ours/main3.py
+++ Ours/main3.py
@@ -16,19 +16,19 @@ from aug import *
 # from cluster import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default='SelfGCon') #SemiGCon
+parser.add_argument('--model', type=str, default='CLGR') #SemiGCon
 parser.add_argument('--dataset', type=str, default='Cora')
 parser.add_argument('--split', type=str, default='PublicSplit') #PublicSplit
-parser.add_argument('--epochs', type=int, default=50)
+parser.add_argument('--epochs', type=int, default=100)
 parser.add_argument('--n_experiments', type=int, default=20)
 parser.add_argument('--n_layers', type=int, default=2) 
 parser.add_argument('--channels', type=int, default=512) 
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr1', type=float, default=1e-3) 
-parser.add_argument('--lr2', type=float, default=5e-3)
+parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd1', type=float, default=1e-5)
 parser.add_argument('--wd2', type=float, default=1e-4)
-parser.add_argument('--fmr', type=float, default=0.2)
+parser.add_argument('--fmr', type=float, default=0.3)
 parser.add_argument('--edr', type=float, default=0.5)
 parser.add_argument('--mlp_use', type=bool, default=False)
 parser.add_argument('--result_file', type=str, default="/Ours/results/Final_accuracy")
@@ -93,9 +93,9 @@ for exp in range(args.n_experiments):
     num_class = int(data.y.max().item()) + 1 
     N = data.num_nodes
 
-    ##### Train the SelfGCon model #####
-    print("=== train SelfGCon model ===")
-    model = SelfGCon(in_dim, hid_dim, out_dim, n_layers, tau, use_mlp = args.mlp_use) #
+    ##### Train CLGR model #####
+    print("=== train CLGR model ===")
+    model = CLGR(in_dim, hid_dim, out_dim, n_layers, tau, use_mlp = args.mlp_use) #
     model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr1, weight_decay=0)
     for epoch in range(args.epochs):
