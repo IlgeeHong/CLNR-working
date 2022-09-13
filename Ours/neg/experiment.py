7a03368452096d11b3d13508dc83281988917f20
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-09-13 12:15:46 -0500

    -

--- Ours/neg/experiment.py
+++ Ours/neg/experiment.py
@@ -16,7 +16,7 @@ from aug import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='CLGR')
-parser.add_argument('--dataset', type=str, default='CS')
+parser.add_argument('--dataset', type=str, default='Physics')
 parser.add_argument('--split', type=str, default='RandomSplit')
 parser.add_argument('--epochs', type=int, default=50) 
 parser.add_argument('--n_experiments', type=int, default=20)
@@ -26,10 +26,10 @@ parser.add_argument('--tau', type=float, default=0.5)
 parser.add_argument('--lr1', type=float, default=1e-3) 
 parser.add_argument('--lr2', type=float, default=5e-3)
 parser.add_argument('--wd1', type=float, default=0.0)
-parser.add_argument('--wd2', type=float, default=1e-2)
-parser.add_argument('--fmr', type=float, default=0.3)
-parser.add_argument('--edr', type=float, default=0.1)
-parser.add_argument('--mlp_use', type=bool, default=True)
+parser.add_argument('--wd2', type=float, default=1e-4)
+parser.add_argument('--fmr', type=float, default=0.2)
+parser.add_argument('--edr', type=float, default=0.4)
+parser.add_argument('--mlp_use', type=bool, default=False)
 parser.add_argument('--result_file', type=str, default="/Ours/neg/results/neg2")
 args = parser.parse_args()
 
@@ -50,7 +50,7 @@ def train(model, data, k=None):
     return loss.item()
 
 results =[]
-for k in [2, 8, 32, 128, 512, None]:
+for k in [2, 8, 32, 128, 512]:
     eval_acc_list = []
     for exp in range(args.n_experiments): 
         if args.split == "PublicSplit":
