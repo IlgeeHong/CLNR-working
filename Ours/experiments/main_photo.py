b40fa6ed1939ef05f04ba00b51695dc2452a6394
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-09-18 16:53:23 -0500

    -

--- Ours/experiments/main_photo.py
+++ Ours/experiments/main_photo.py
@@ -16,7 +16,7 @@ from aug import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='CLGR')
-parser.add_argument('--dataset', type=str, default='Computers')
+parser.add_argument('--dataset', type=str, default='Photo')
 parser.add_argument('--n_experiments', type=int, default=20)
 parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--tau', type=float, default=0.5) 
