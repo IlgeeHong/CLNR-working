016ac7c8df42ee480f79f70281590eda9b19d795
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-09-20 13:47:34 -0500

    -

--- Ours/ablation/main.py
+++ Ours/ablation/main.py
@@ -17,7 +17,7 @@ from cluster import *
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='CLGR') 
 parser.add_argument('--dataset', type=str, default='Photo')
-parser.add_argument('--n_experiments', type=int, default=1)
+parser.add_argument('--n_experiments', type=int, default=20)
 parser.add_argument('--epochs', type=int, default=100)
 parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--tau', type=float, default=0.5) 
