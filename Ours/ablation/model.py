06ecb8c7ac3722fb4aec516cb6bf3b0e4d06c9e2
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-09-28 22:15:29 -0500

    -

--- Ours/ablation/model.py
+++ Ours/ablation/model.py
@@ -4,6 +4,7 @@
 # In[ ]:
 
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import random
@@ -112,14 +113,17 @@ class CLGR(nn.Module):
         ret = ret.mean() if mean else ret.sum()
         return ret
 
-class SupCLGR(nn.Module):
-    def __init__(self, in_dim, hid_dim, out_dim, n_layers, tau = 0.5, use_mlp = False):
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class CCA_SSG(nn.Module):
+    def __init__(self, in_dim, hid_dim, out_dim, n_layers, lambd, N, use_mlp = False):
         super().__init__()
         if not use_mlp:
             self.backbone = GCN(in_dim, hid_dim, out_dim, n_layers)
         else:
             self.backbone = MLP(in_dim, hid_dim, out_dim)
-        self.tau = tau
+        self.lambd = lambd
+        self.N = N
 
     def get_embedding(self, data):
         out = self.backbone(data.x, data.edge_index)
@@ -132,63 +136,147 @@ class SupCLGR(nn.Module):
         z2 = (h2 - h2.mean(0)) / h2.std(0)
         return z1, z2
     
-    def sim(self, z1, z2, pos_idx, indices):
+    def loss(self, z1, z2):
+        c = torch.mm(z1.T, z2)
+        c1 = torch.mm(z1.T, z1)
+        c2 = torch.mm(z2.T, z2)
+        c = c / self.N
+        c1 = c1 / self.N
+        c2 = c2 / self.N
+        loss_inv = - torch.diagonal(c).sum()
+        iden = torch.tensor(np.eye(c.shape[0])).to(device)
+        loss_dec1 = (iden - c1).pow(2).sum()
+        loss_dec2 = (iden - c2).pow(2).sum()
+        lambd = self.lambd
+        ret = loss_inv + lambd * (loss_dec1 + loss_dec2)
+        return ret
+
+
+class GRACE(nn.Module):
+    def __init__(self, in_dim, hid_dim, proj_hid_dim, n_layers, tau = 0.5, use_mlp = False):
+        super().__init__()
+        if not use_mlp:
+            self.backbone = GCN(in_dim, hid_dim, hid_dim, n_layers)
+        else:
+            self.backbone = MLP(in_dim, hid_dim, hid_dim)
+
+        self.fc1 = nn.Linear(hid_dim, proj_hid_dim)
+        self.fc2 = nn.Linear(proj_hid_dim, hid_dim)
+        self.fc3 = nn.Linear(hid_dim, hid_dim)
+        self.tau = tau
+        
+    def get_embedding(self, data):
+        out = self.backbone(data.x, data.edge_index)
+        return out.detach()
+
+    def forward(self, data1, data2):
+        z1 = self.backbone(data1.x, data1.edge_index)
+        z2 = self.backbone(data2.x, data2.edge_index)
+        return z1, z2
+    
+    def projection(self, z):
+        z = F.elu(self.fc1(z))
+        h = self.fc2(z)
+        return h
+    
+    def sim(self, z1, z2):
         z1 = F.normalize(z1)
         z2 = F.normalize(z2)
-        f = lambda x: torch.exp(x / self.tau) 
-        if indices is not None:
-            z2_new = z2[indices,:]
-            sim = f(torch.mm(z1, z2_new.t()))
-            diag = f(torch.mm(z1,z2.t()).diag())
-            sim_pos_temp1 = f(torch.mm(z1, z2.t()))
-            sim_pos_temp2 = sim_pos_temp1.clone()
-            sim_pos_temp2[~pos_idx] = 0
-            sim_pos = sim_pos_temp2.clone()
-            sim_pos_sum = sim_pos.sum(1)
-        else:
-            sim = f(torch.mm(z1, z2.t()))
-            diag = f(torch.mm(z1, z2.t()).diag())
-            sim_pos_temp1 = sim.clone()
-            sim_pos_temp1[~pos_idx] = 0
-            sim_pos = sim_pos_temp1.clone()
-            sim_pos_sum = sim_pos.sum(1)
-        return sim, diag, sim_pos_sum
+        return torch.mm(z1, z2.t())
 
-    def semi_loss(self, data, z1, z2, num_class, train_idx, indices):
-        class_idx = []
-        for c in range(num_class):
-            index = (data.y == c) * train_idx
-            class_idx.append(index)
-        class_idx = torch.stack(class_idx).bool()
-        pos_idx = class_idx[data.y]
-        pos_idx[~train_idx] = False
-        pos_idx.fill_diagonal_(True)
-        
-        refl_sim, refl_diag, refl_pos_sum = self.sim(z1, z1, pos_idx, indices)
-        between_sim, _, between_pos_sum = self.sim(z1, z2, pos_idx, indices)
-        num_per_class = pos_idx.sum(1)
+    def semi_loss(self, z1, z2):
+        f = lambda x: torch.exp(x / self.tau)
+        refl_sim = f(self.sim(z1, z1))
+        between_sim = f(self.sim(z1, z2))
+        return -torch.log(between_sim.diag() / (refl_sim.sum(1) + between_sim.sum(1) - refl_sim.diag()))
 
-        if indices is not None:
-            refl_diag_temp = refl_diag.clone()
-            refl_diag_temp[~indices] = 0.0
-            refl_diag_neg = refl_diag_temp.clone()
-        else:
-            refl_diag_temp = refl_diag.clone()
-            refl_diag_neg = refl_diag_temp.clone()
-        
-        semi_loss = -(1/(2*num_per_class-1))*torch.log(
-            (between_pos_sum + refl_pos_sum - refl_diag) / (between_sim.sum(1) + refl_sim.sum(1) - refl_diag_neg)
-            )
-        return semi_loss
-        
-    def loss(self, data, z1, z2, num_class, train_idx, k=None, mean=True):
-        if k is not None:
-            N = z1.shape[0]
-            indices = torch.LongTensor(random.sample(range(N), k))   
-        else:
-            indices = None
-        l1 = self.semi_loss(data, z1, z2, num_class, train_idx, indices)
-        l2 = self.semi_loss(data, z2, z1, num_class, train_idx, indices)
+    def loss(self, z1, z2, mean = True):
+        h1 = self.projection(z1)
+        h2 = self.projection(z2)
+        l1 = self.semi_loss(h1, h2)
+        l2 = self.semi_loss(h2, h1)
         ret = (l1 + l2) * 0.5
         ret = ret.mean() if mean else ret.sum()
         return ret
+
+
+# class SupCLGR(nn.Module):
+#     def __init__(self, in_dim, hid_dim, out_dim, n_layers, tau = 0.5, use_mlp = False):
+#         super().__init__()
+#         if not use_mlp:
+#             self.backbone = GCN(in_dim, hid_dim, out_dim, n_layers)
+#         else:
+#             self.backbone = MLP(in_dim, hid_dim, out_dim)
+#         self.tau = tau
+
+#     def get_embedding(self, data):
+#         out = self.backbone(data.x, data.edge_index)
+#         return out.detach()
+
+#     def forward(self, data1, data2):
+#         h1 = self.backbone(data1.x, data1.edge_index)
+#         h2 = self.backbone(data2.x, data2.edge_index)
+#         z1 = (h1 - h1.mean(0)) / h1.std(0)
+#         z2 = (h2 - h2.mean(0)) / h2.std(0)
+#         return z1, z2
+    
+#     def sim(self, z1, z2, pos_idx, indices):
+#         z1 = F.normalize(z1)
+#         z2 = F.normalize(z2)
+#         f = lambda x: torch.exp(x / self.tau) 
+#         if indices is not None:
+#             z2_new = z2[indices,:]
+#             sim = f(torch.mm(z1, z2_new.t()))
+#             diag = f(torch.mm(z1,z2.t()).diag())
+#             sim_pos_temp1 = f(torch.mm(z1, z2.t()))
+#             sim_pos_temp2 = sim_pos_temp1.clone()
+#             sim_pos_temp2[~pos_idx] = 0
+#             sim_pos = sim_pos_temp2.clone()
+#             sim_pos_sum = sim_pos.sum(1)
+#         else:
+#             sim = f(torch.mm(z1, z2.t()))
+#             diag = f(torch.mm(z1, z2.t()).diag())
+#             sim_pos_temp1 = sim.clone()
+#             sim_pos_temp1[~pos_idx] = 0
+#             sim_pos = sim_pos_temp1.clone()
+#             sim_pos_sum = sim_pos.sum(1)
+#         return sim, diag, sim_pos_sum
+
+#     def semi_loss(self, data, z1, z2, num_class, train_idx, indices):
+#         class_idx = []
+#         for c in range(num_class):
+#             index = (data.y == c) * train_idx
+#             class_idx.append(index)
+#         class_idx = torch.stack(class_idx).bool()
+#         pos_idx = class_idx[data.y]
+#         pos_idx[~train_idx] = False
+#         pos_idx.fill_diagonal_(True)
+        
+#         refl_sim, refl_diag, refl_pos_sum = self.sim(z1, z1, pos_idx, indices)
+#         between_sim, _, between_pos_sum = self.sim(z1, z2, pos_idx, indices)
+#         num_per_class = pos_idx.sum(1)
+
+#         if indices is not None:
+#             refl_diag_temp = refl_diag.clone()
+#             refl_diag_temp[~indices] = 0.0
+#             refl_diag_neg = refl_diag_temp.clone()
+#         else:
+#             refl_diag_temp = refl_diag.clone()
+#             refl_diag_neg = refl_diag_temp.clone()
+        
+#         semi_loss = -(1/(2*num_per_class-1))*torch.log(
+#             (between_pos_sum + refl_pos_sum - refl_diag) / (between_sim.sum(1) + refl_sim.sum(1) - refl_diag_neg)
+#             )
+#         return semi_loss
+        
+#     def loss(self, data, z1, z2, num_class, train_idx, k=None, mean=True):
+#         if k is not None:
+#             N = z1.shape[0]
+#             indices = torch.LongTensor(random.sample(range(N), k))   
+#         else:
+#             indices = None
+#         l1 = self.semi_loss(data, z1, z2, num_class, train_idx, indices)
+#         l2 = self.semi_loss(data, z2, z1, num_class, train_idx, indices)
+#         ret = (l1 + l2) * 0.5
+#         ret = ret.mean() if mean else ret.sum()
+#         return ret
