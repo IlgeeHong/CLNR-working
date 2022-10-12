5284e1ff2685ecaa3f1b2b8286b0625c3478d2c1
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-10-12 18:09:49 -0500

    -

--- GRACE/main_pubmed.py
+++ GRACE/main_pubmed.py
@@ -18,9 +18,9 @@ from aug import *
 # from cluster import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default='GRACE')
+parser.add_argument('--model', type=str, default='dCLNR')
 parser.add_argument('--dataset', type=str, default='PubMed')
-# parser.add_argument('--split', type=str, default='PublicSplit')
+parser.add_argument('--epochs', type=int, default=200)
 parser.add_argument('--n_experiments', type=int, default=20)
 parser.add_argument('--n_layers', type=int, default=2) 
 parser.add_argument('--channels', type=int, default=512)
@@ -33,7 +33,8 @@ parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--edr', type=float, default=0.5)
 parser.add_argument('--fmr', type=float, default=0.3)
 parser.add_argument('--proj', type=str, default="dbn")
-parser.add_argument('--result_file', type=str, default="/GRACE/results/new")
+parser.add_argument('--result_file', type=str, default="/GRACE/results/final")
+# parser.add_argument('--result_file', type=str, default="/GRACE/results/new")
 # parser.add_argument('--embeddings', type=str, default="/results/GRACE_node_classification_embeddings")
 args = parser.parse_args()
 
@@ -54,73 +55,69 @@ def train(model, data, fmr, edr, proj):
     return loss.item()
 
 results =[]
-for epochs in [50, 100, 200, 400, 600]: #, 800, 1000, 1500, 2000
-    eval_acc_list = []
-    for exp in range(args.n_experiments):      
-        data, train_idx, val_idx, test_idx = load(args.dataset, device)
-        in_dim = data.num_features
-        hid_dim = args.channels
-        proj_hid_dim = args.proj_hid_dim
-        n_layers = args.n_layers
-        tau = args.tau
-        num_class = int(data.y.max().item()) + 1 
-        N = data.num_nodes
-        ##### Train GRACE model #####
-        print("=== train GRACE model ===")
-        model = GRACE(in_dim, hid_dim, proj_hid_dim, n_layers, tau)
-        model = model.to(device)
-        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr1, weight_decay=args.wd1)
-        for epoch in range(epochs):
-            loss = train(model, data, args.fmr, args.edr, args.proj)
-            print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss))
-        embeds = model.get_embedding(data)
-        train_embs = embeds[train_idx]
-        val_embs = embeds[val_idx]
-        test_embs = embeds[test_idx]
-        label = data.y
-        label = label.to(device)
-        feat = data.x
-        train_labels = label[train_idx]
-        val_labels = label[val_idx]
-        test_labels = label[test_idx]
-        train_feat = feat[train_idx]
-        val_feat = feat[val_idx]
-        test_feat = feat[test_idx] 
-        ''' Linear Evaluation '''
-        logreg = LogReg(train_embs.shape[1], num_class)
-        logreg = logreg.to(device)
-        opt = torch.optim.Adam(logreg.parameters(), lr=args.lr2, weight_decay=args.wd2)
-        loss_fn = nn.CrossEntropyLoss()
-        best_val_acc = 0
-        eval_acc = 0
-        for epoch in range(2000):
-            logreg.train()
-            opt.zero_grad()
-            logits = logreg(train_embs)
-            preds = torch.argmax(logits, dim=1)
-            train_acc = torch.sum(preds == train_labels).float() / train_labels.shape[0]
-            loss = loss_fn(logits, train_labels)
-            loss.backward()
-            opt.step()
-
-            logreg.eval()
-            with torch.no_grad():
-                val_logits = logreg(val_embs)
-                test_logits = logreg(test_embs)
-
-                val_preds = torch.argmax(val_logits, dim=1)
-                test_preds = torch.argmax(test_logits, dim=1)
-
-                val_acc = torch.sum(val_preds == val_labels).float() / val_labels.shape[0]
-                test_acc = torch.sum(test_preds == test_labels).float() / test_labels.shape[0]
-
-                if val_acc >= best_val_acc:
-                    best_val_acc = val_acc
-                    if test_acc > eval_acc:
-                        eval_acc = test_acc
-        eval_acc_list.append(eval_acc.item())
-    eval_acc_mean = mean(eval_acc_list)
-    results += [[args.model, args.dataset, epochs, args.lr1, args.lr2, args.wd1, args.wd2, args.channels, args.tau, args.edr, args.fmr, eval_acc_mean]]
+# for epochs in [50, 100, 200, 400, 600]: #, 800, 1000, 1500, 2000
+#    eval_acc_list = []
+for exp in range(args.n_experiments):      
+    data, train_idx, val_idx, test_idx = load(args.dataset, device)
+    in_dim = data.num_features
+    hid_dim = args.channels
+    proj_hid_dim = args.proj_hid_dim
+    n_layers = args.n_layers
+    tau = args.tau
+    num_class = int(data.y.max().item()) + 1 
+    N = data.num_nodes
+    ##### Train GRACE model #####
+    print("=== train GRACE model ===")
+    model = GRACE(in_dim, hid_dim, proj_hid_dim, n_layers, tau)
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr1, weight_decay=args.wd1)
+    for epoch in range(args.epochs):
+        loss = train(model, data, args.fmr, args.edr, args.proj)
+        print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss))
+    embeds = model.get_embedding(data)
+    train_embs = embeds[train_idx]
+    val_embs = embeds[val_idx]
+    test_embs = embeds[test_idx]
+    label = data.y
+    label = label.to(device)
+    feat = data.x
+    train_labels = label[train_idx]
+    val_labels = label[val_idx]
+    test_labels = label[test_idx]
+    train_feat = feat[train_idx]
+    val_feat = feat[val_idx]
+    test_feat = feat[test_idx] 
+    ''' Linear Evaluation '''
+    logreg = LogReg(train_embs.shape[1], num_class)
+    logreg = logreg.to(device)
+    opt = torch.optim.Adam(logreg.parameters(), lr=args.lr2, weight_decay=args.wd2)
+    loss_fn = nn.CrossEntropyLoss()
+    best_val_acc = 0
+    eval_acc = 0
+    for epoch in range(2000):
+        logreg.train()
+        opt.zero_grad()
+        logits = logreg(train_embs)
+        preds = torch.argmax(logits, dim=1)
+        train_acc = torch.sum(preds == train_labels).float() / train_labels.shape[0]
+        loss = loss_fn(logits, train_labels)
+        loss.backward()
+        opt.step()
+        logreg.eval()
+        with torch.no_grad():
+            val_logits = logreg(val_embs)
+            test_logits = logreg(test_embs)
+            val_preds = torch.argmax(val_logits, dim=1)
+            test_preds = torch.argmax(test_logits, dim=1)
+            val_acc = torch.sum(val_preds == val_labels).float() / val_labels.shape[0]
+            test_acc = torch.sum(test_preds == test_labels).float() / test_labels.shape[0]
+            if val_acc >= best_val_acc:
+                best_val_acc = val_acc
+                if test_acc > eval_acc:
+                    eval_acc = test_acc
+        # eval_acc_list.append(eval_acc.item())
+    # eval_acc_mean = mean(eval_acc_list)
+    results += [[args.model, args.dataset, args.epochs, args.lr1, args.lr2, args.wd1, args.wd2, args.channels, args.tau, args.edr, args.fmr, eval_acc.item()]]
     res1 = pd.DataFrame(results, columns=['model', 'dataset', 'epochs', 'lr1', 'lr2', 'wd1', 'wd2', 'channels', 'tau', 'edr', 'fmr', 'accuracy'])
     res1.to_csv(file_path + "_" + args.model + "_" + args.dataset + "_" + str(args.channels) + ".csv", index=False)
 
