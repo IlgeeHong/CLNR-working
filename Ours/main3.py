926e44fdc4a38a5973af9876d33dee5cab12bd70
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-10-13 08:01:51 -0500

    -

--- Ours/main3.py
+++ Ours/main3.py
@@ -141,10 +141,11 @@ for epochs in [2000, 4000, 6000, 8000, 10000]:
                         eval_acc = test_acc
         eval_acc_list.append(eval_acc.item())
     eval_acc_mean = mean(eval_acc_list)
-    # eval_acc_std = stdev(eval_acc_list)
+    eval_acc_std = stdev(eval_acc_list)
+
     # print('Epoch:{}, train_acc:{:.4f}, val_acc:{:4f}, test_acc:{:4f}'.format(epoch, train_acc, val_acc, test_acc))
     # print('Linear evaluation accuracy:{:.4f}'.format(eval_acc))
     # print('Linear evaluation accuracy:{:.4f}'.format(eval_acc))
-    results += [[args.model, recored_time, args.dataset, epochs, args.n_layers, args.tau, args.lr1, args.lr2, args.wd1, args.wd2, args.channels, args.edr, args.fmr, eval_acc_mean]]
-    res1 = pd.DataFrame(results, columns=['model', 'Time', 'dataset', 'epochs', 'layers', 'tau', 'lr1', 'lr2', 'wd1', 'wd2', 'channels', 'edge_drop_rate', 'feat_mask_rate', 'accuracy'])
+    results += [[args.model, recored_time, args.dataset, epochs, args.n_layers, args.tau, args.lr1, args.lr2, args.wd1, args.wd2, args.channels, args.edr, args.fmr, eval_acc_mean, eval_acc_std]]
+    res1 = pd.DataFrame(results, columns=['model', 'Time', 'dataset', 'epochs', 'layers', 'tau', 'lr1', 'lr2', 'wd1', 'wd2', 'channels', 'edge_drop_rate', 'feat_mask_rate', 'accuracy', 'stdev'])
     res1.to_csv(file_path + "_" + args.model + "_" + args.dataset +  ".csv", index=False)
