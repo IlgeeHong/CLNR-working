4aeb9df7a981c2c63e7f67ea160245e2edc2092e
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-10-13 10:17:11 -0500

    -

--- Ours/main3.py
+++ Ours/main3.py
@@ -79,7 +79,7 @@ for exp in range(args.n_experiments):
     model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr1, weight_decay=args.wd1)
     for epoch in range(args.epochs):
-        loss = train(model, args.fmr, args.edr, data, k=64)
+        loss = train(model, args.fmr, args.edr, data, k=2048)
         # loss = train(model, data)
         print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss))
     end.record()
