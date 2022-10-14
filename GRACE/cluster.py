1985c437107c0f2ee65de38293d45981caecf2f3
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-10-13 20:17:39 -0500

    -

--- GRACE/cluster.py
+++ GRACE/cluster.py
@@ -36,7 +36,7 @@ def visualize_pca(out, color, pc1, pc2, path, model, size=30, epoch = None, loss
     plt.title("EVR:"+  str(round(evr[pc1],4)) + " " + "vs" + " " + str(round(evr[pc2],4)), fontsize = 20)
     if epoch is not None and loss is not None:
         plt.xlabel(f'Epoch: {epoch}, Loss: {loss:.4f}', fontsize=20)
-    plt.savefig(path + "_" + model + "_" + 'cora_pc' + str(pc1) + str(pc2) + '.png')    
+    plt.savefig(path + "_" + model + "_" + 'cora_pc' + str(pc1) + str(pc2) + '.png',bbox_inches='tight')    
     # plt.show()
 
 # def visualize_tsne(out, color, size=30, epoch=None, loss = None):
