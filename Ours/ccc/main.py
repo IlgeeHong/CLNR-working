56da939ad10f5dae8145d4516adf609bc6d5138b
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-07 15:02:00 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -16,7 +16,7 @@ from dataset import *
 from statistics import mean, stdev
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default='CLNR') 
+parser.add_argument('--model', type=str, default='bCLNR') 
 parser.add_argument('--dataset', type=str, default='Cora') 
 parser.add_argument('--n_experiments', type=int, default=20)
 parser.add_argument('--epochs', type=int, default=50)
@@ -29,7 +29,7 @@ parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--channels', type=int, default=512) 
 parser.add_argument('--fmr', type=float, default=0.2)
 parser.add_argument('--edr', type=float, default=0.5)
-parser.add_argument('--batch', type=int, default=1024) #None
+parser.add_argument('--batch', type=int, default=256) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
 parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/")
 args = parser.parse_args()
