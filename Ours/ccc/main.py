601b2b207d4618027b7db3a79ec6707d2a49b2b3
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-14 22:00:27 -0600

    out=2

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -16,9 +16,9 @@ from dataset import *
 from statistics import mean, stdev
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default='bCLNR') 
+parser.add_argument('--model', type=str, default='CLNR') 
 parser.add_argument('--dataset', type=str, default='Cora') 
-parser.add_argument('--n_experiments', type=int, default=20)
+parser.add_argument('--n_experiments', type=int, default=1)
 parser.add_argument('--epochs', type=int, default=50)
 parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--tau', type=float, default=0.5) 
@@ -26,10 +26,11 @@ parser.add_argument('--lr1', type=float, default=1e-3)
 parser.add_argument('--wd1', type=float, default=0.0)
 parser.add_argument('--lr2', type=float, default=5e-3)
 parser.add_argument('--wd2', type=float, default=1e-4)
-parser.add_argument('--channels', type=int, default=512) 
+parser.add_argument('--hid_dim', type=int, default=512)
+parser.add_argument('--out_dim', type=int, default=2) 
 parser.add_argument('--fmr', type=float, default=0.2)
 parser.add_argument('--edr', type=float, default=0.5)
-parser.add_argument('--batch', type=int, default=1024) #None
+parser.add_argument('--batch', type=int, default=None) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
 parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/")
 args = parser.parse_args()
@@ -49,4 +50,4 @@ eval_acc_mean = mean(eval_acc_list)
 eval_acc_std = stdev(eval_acc_list)
 results += [[args.model, args.dataset, args.epochs, args.n_layers, args.tau, args.lr1, args.lr2, args.wd1, args.wd2, args.channels, args.edr, args.fmr, eval_acc_mean, eval_acc_std]]
 res = pd.DataFrame(results, columns=['model', 'dataset', 'epochs', 'layers', 'tau', 'lr1', 'lr2', 'wd1', 'wd2', 'channels', 'edge_drop_rate', 'feat_mask_rate', 'mean', 'std'])
-res.to_csv(file_path + args.model + "_" + str(args.batch) + str(args.epochs) + "_" + args.dataset +  ".csv", index=False)
+res.to_csv(file_path + args.model + "_" + str(args.batch) + "_" + str(args.epochs) + "_" + args.dataset +  ".csv", index=False)
