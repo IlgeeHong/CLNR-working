e3e451b1cc5abdb62b37db7c897034660d17862f
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-19 15:55:12 -0600

    main

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -24,9 +24,9 @@ parser.add_argument('--tau', type=float, default=0.5)
 parser.add_argument('--lr2', type=float, default=5e-3)
 parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--hid_dim', type=int, default=512)
-parser.add_argument('--out_dim', type=int, default=512) 
-parser.add_argument('--fmr', type=float, default=0.2) #0.1 #0.2
-parser.add_argument('--edr', type=float, default=0.5) #0.4 #0.5
+parser.add_argument('--out_dim', type=int, default=256) 
+parser.add_argument('--fmr', type=float, default=0.1) #0.1 #0.2
+parser.add_argument('--edr', type=float, default=0.4) #0.4 #0.5
 parser.add_argument('--lambd', type=float, default=1e-3)
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
@@ -49,22 +49,16 @@ for args.model in ['CLNR','bCLNR','bCLNR2','dCLNR','dCLNR2','GRACE','CCA-SSG']:
         args.epochs = 50
         args.lr1 = 1e-3
         args.wd1 = 0.0
-        args.hid_dim = 512
-        args.out_dim = 512
         args.loss_type = 'ntxent'
     elif args.model in ['GRACE']:
         args.epochs = 400
         args.lr1 = 5e-4
         args.wd1 = 1e-4
-        args.hid_dim = 512
-        args.out_dim = 512
         args.loss_type = 'ntxent'
     elif args.model in ['CCA-SSG']:
         args.epochs = 50
         args.lr1 = 1e-3
         args.wd1 = 0.0
-        args.hid_dim = 512
-        args.out_dim = 512
         args.loss_type = 'cca'
 
     eval_acc_list = []
