8f228ac3cb5a5a0b84e340612fa7da14fa356161
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-19 21:35:00 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -25,16 +25,16 @@ from statistics import mean, stdev
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='Photo') 
+parser.add_argument('--dataset', type=str, default='Physics') 
 parser.add_argument('--n_experiments', type=int, default=20)
 parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--tau', type=float, default=0.5) 
-parser.add_argument('--lr2', type=float, default=1e-2)
+parser.add_argument('--lr2', type=float, default=5e-3)
 parser.add_argument('--wd2', type=float, default=1e-4)
-parser.add_argument('--hid_dim', type=int, default=256)
-parser.add_argument('--out_dim', type=int, default=256) 
-parser.add_argument('--fmr', type=float, default=0.0) #0.1 #0.2
-parser.add_argument('--edr', type=float, default=0.5) #0.4 #0.5
+parser.add_argument('--hid_dim', type=int, default=512)
+parser.add_argument('--out_dim', type=int, default=512) 
+parser.add_argument('--fmr', type=float, default=0.2) #0.1 #0.2
+parser.add_argument('--edr', type=float, default=0.4) #0.4 #0.5
 parser.add_argument('--lambd', type=float, default=1e-3)
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
@@ -74,7 +74,7 @@ for args.model in ['nCLNR','CLNR','bCLNR','dCLNR','CLNR-unif','CLNR-align','nCLN
         args.wd1 = 1e-5
         args.loss_type = 'ntxent'
     elif args.model in ['CCA-SSG']:
-        args.epochs = 50
+        args.epochs = 100
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'cca'
