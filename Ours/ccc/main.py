1ea6f684e45ace2b1b4c2bac063400a6eb561b29
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-19 21:56:23 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -25,18 +25,18 @@ from statistics import mean, stdev
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='Physics') 
+parser.add_argument('--dataset', type=str, default='Computers') 
 parser.add_argument('--n_experiments', type=int, default=20)
 parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--tau', type=float, default=0.5) 
-parser.add_argument('--lr2', type=float, default=5e-3)
+parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=512) 
-parser.add_argument('--fmr', type=float, default=0.2) #0.1 #0.2
-parser.add_argument('--edr', type=float, default=0.4) #0.4 #0.5
+parser.add_argument('--fmr', type=float, default=0.0) #0.1 #0.2
+parser.add_argument('--edr', type=float, default=0.5) #0.4 #0.5
 parser.add_argument('--lambd', type=float, default=1e-3)
-parser.add_argument('--batch', type=int, default=64) #None
+parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
 parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/")
 # parser.add_argument('--model', type=str, default='CLNR')
@@ -54,27 +54,27 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 results =[]
 for args.model in ['nCLNR','CLNR','bCLNR','dCLNR','CLNR-unif','CLNR-align','nCLNR-align','GRACE','CCA-SSG']:
     if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
-        args.epochs = 100
+        args.epochs = 200
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['CLNR-unif']:
-        args.epochs = 100
+        args.epochs = 200
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent-uniform'
     elif args.model in ['CLNR-align','nCLNR-align']:
-        args.epochs = 100
+        args.epochs = 200
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent-align'
     elif args.model in ['GRACE']:
-        args.epochs = 2000
+        args.epochs = 1500
         args.lr1 = 1e-3
-        args.wd1 = 1e-5
+        args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['CCA-SSG']:
-        args.epochs = 100
+        args.epochs = 50
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'cca'
