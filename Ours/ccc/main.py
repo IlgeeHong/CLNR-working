22a42a497a7697fd95155fd8b02263d5b679773b
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-20 21:17:26 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -26,7 +26,7 @@ from statistics import mean, stdev
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='Cora') 
-parser.add_argument('--n_experiments', type=int, default=1)
+parser.add_argument('--n_experiments', type=int, default=3)
 parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=5e-3)
