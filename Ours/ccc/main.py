2d68d65736957a66000747ae75ac9968a2e24b26
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-21 17:21:56 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -84,8 +84,8 @@ for args.model in ['CLNR']:#
     uniformity_list = []
     alignment_list = [] 
     for exp in range(args.n_experiments):
+        print(args.epochs)
         data, train_idx, val_idx, test_idx = load(args.dataset, device)
-        print(data.shape)
         model = ContrastiveLearning(args, data, device)
         model.train()
         eval_acc, Lu, La = model.LinearEvaluation(train_idx, val_idx, test_idx)
