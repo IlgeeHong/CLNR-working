df6ccddace12275854f18787c7b47a4f44cb1ce7
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-26 20:05:03 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -34,7 +34,7 @@ parser.add_argument('--out_dim', type=int, default=512)
 parser.add_argument('--fmr', type=float, default=0.2) #0.1 #0.2
 parser.add_argument('--edr', type=float, default=0.5) #0.4 #0.5
 parser.add_argument('--lambd', type=float, default=1e-3)
-parser.add_argument('--batch', type=int, default=1024) #None
+parser.add_argument('--batch', type=int, default=4) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
 parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/")
 # parser.add_argument('--model', type=str, default='CLNR')
