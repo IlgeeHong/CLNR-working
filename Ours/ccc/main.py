51a9a5f651a3eb61eda7b5692802e26c45a0a412
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-29 09:53:04 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -25,7 +25,7 @@ from statistics import mean, stdev
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='ogbn-arxiv') #ogbn-arxiv
 parser.add_argument('--n_experiments', type=int, default=2)
-parser.add_argument('--n_layers', type=int, default=3)
+parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd2', type=float, default=1e-4)
@@ -53,7 +53,7 @@ results =[]
 # for args.model in ['CLNR-align','nCLNR','CLNR','bCLNR','dCLNR','GRACE','CCA-SSG']: #'CLNR-unif',
 for args.model in ['CLNR']:
     if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
-        args.epochs = 8000
+        args.epochs = 5000
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
