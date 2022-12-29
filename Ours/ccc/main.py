4fd0845d574a80dd61b453aba53d42ccbe91d082
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-29 13:29:04 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -25,12 +25,12 @@ from statistics import mean, stdev
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='ogbn-arxiv') #ogbn-arxiv
 parser.add_argument('--n_experiments', type=int, default=2)
-parser.add_argument('--n_layers', type=int, default=2)
+parser.add_argument('--n_layers', type=int, default=3)
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd2', type=float, default=1e-4)
-parser.add_argument('--hid_dim', type=int, default=1024)
-parser.add_argument('--out_dim', type=int, default=1024) 
+parser.add_argument('--hid_dim', type=int, default=512)
+parser.add_argument('--out_dim', type=int, default=512) 
 parser.add_argument('--fmr', type=float, default=0.0) #0.1 #0.2
 parser.add_argument('--edr', type=float, default=0.6) #0.4 #0.5
 parser.add_argument('--lambd', type=float, default=1e-3) # citeseer, computer 5e-4
