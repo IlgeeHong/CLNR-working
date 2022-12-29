3950fa6302697ef39c267dc3f66a431b037f66f2
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-28 18:58:49 -0600

    -

--- Ours/ccc/model.py
+++ Ours/ccc/model.py
@@ -248,10 +248,8 @@ class ContrastiveLearning(nn.Module):
         # self.model = self.model.cpu()
         # embeds = self.model.get_embedding(self.data)
         # embeds = embeds.to(self.device)
-        self.model = self.model.cpu()
-        # embeds = self.model.get_embedding(self.data.to(self.device))
-        embeds = self.model.get_embedding(self.data)
-        embeds = embeds.to(self.device)
+        torch.cuda.empty_cache()
+        embeds = self.model.get_embedding(self.data.to(self.device))
         train_embs = embeds[train_idx]
         val_embs = embeds[val_idx]
         test_embs = embeds[test_idx]
