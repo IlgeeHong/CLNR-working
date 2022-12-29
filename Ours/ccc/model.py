1f7d7535f4ac5a460a03afea76a87e4d9ce7551f
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-28 18:37:11 -0600

    -

--- Ours/ccc/model.py
+++ Ours/ccc/model.py
@@ -276,7 +276,7 @@ class ContrastiveLearning(nn.Module):
             self.logreg.train()
             self.opt.zero_grad()
             logits = self.logreg(train_embs)
-            print(train_embs.shape)
+            print(train_labels.shape)
             # preds = torch.argmax(logits, dim=1)
             # train_acc = torch.sum(preds == train_labels).float() / train_labels.shape[0]
             loss = loss_fn(logits, train_labels)
