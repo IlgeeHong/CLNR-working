efee83d89aedf1128bbcb373ed2528d2ce70d971
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-30 00:07:17 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -23,7 +23,7 @@ from statistics import mean, stdev
 # photo : 2000 / 1e-3 / 1e-5 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='Computers') #ogbn-arxiv
+parser.add_argument('--dataset', type=str, default='Photo') #ogbn-arxiv
 parser.add_argument('--n_experiments', type=int, default=20) #2
 parser.add_argument('--n_layers', type=int, default=2) #3
 parser.add_argument('--tau', type=float, default=0.5) 
@@ -33,7 +33,7 @@ parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=512) 
 parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5
-parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer 5e-4
+parser.add_argument('--lambd', type=float, default=1e-3) # citeseer, computer 5e-4
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
 parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/")
@@ -53,22 +53,22 @@ results =[]
 for args.model in ['CLNR-align','nCLNR','CLNR','bCLNR','dCLNR','GRACE','CCA-SSG']: #'CLNR-unif',
 # for args.model in ['CLNR']:
     if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
-        args.epochs = 200 # 20000
+        args.epochs = 100 # 20000
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['CLNR-unif']:
-        args.epochs = 200
+        args.epochs = 100
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent-uniform'
     elif args.model in ['CLNR-align','nCLNR-align']:
-        args.epochs = 200
+        args.epochs = 100
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent-align'
     elif args.model in ['GRACE']:
-        args.epochs = 200
+        args.epochs = 100
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
