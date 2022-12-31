c577b5aa81740a60e2b5048ad53c1a6cc6afd345
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2022-12-31 14:43:13 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -23,11 +23,11 @@ from statistics import mean, stdev
 # photo : 2000 / 1e-3 / 1e-5 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='Cora') #
+parser.add_argument('--dataset', type=str, default='Circles') #
 parser.add_argument('--n_experiments', type=int, default=20) #2
 parser.add_argument('--n_layers', type=int, default=2) #3
 parser.add_argument('--tau', type=float, default=0.5) 
-parser.add_argument('--lr2', type=float, default=5e-3)
+parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=512) 
@@ -53,28 +53,28 @@ results =[]
 for args.model in ['CLNR-align','nCLNR','CLNR','bCLNR','dCLNR','GRACE','CCA-SSG']: #'CLNR-unif',
 # for args.model in ['CLNR']:
     if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
-        args.epochs = 50 # 10000
-        args.lr1 = 1e-3 # 1e-2
+        args.epochs = 100 # 10000
+        args.lr1 = 1e-2 # 1e-2
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['CLNR-unif']:
-        args.epochs = 50
-        args.lr1 = 1e-3
+        args.epochs = 100
+        args.lr1 = 1e-2
         args.wd1 = 0.0
         args.loss_type = 'ntxent-uniform'
     elif args.model in ['CLNR-align','nCLNR-align']:
-        args.epochs = 50
-        args.lr1 = 1e-3
+        args.epochs = 100
+        args.lr1 = 1e-2
         args.wd1 = 0.0
         args.loss_type = 'ntxent-align'
     elif args.model in ['GRACE']:
-        args.epochs = 50
-        args.lr1 = 1e-3
+        args.epochs = 100
+        args.lr1 = 1e-2
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['CCA-SSG']:
-        args.epochs = 50
-        args.lr1 = 1e-3
+        args.epochs = 100
+        args.lr1 = 1e-2
         args.wd1 = 0.0
         args.loss_type = 'cca'
 
