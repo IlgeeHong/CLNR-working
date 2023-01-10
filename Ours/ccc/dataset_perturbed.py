76f757a68d55f8f46560b64ca36b0d44405bc470
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-01-09 18:00:55 -0600

    -

--- Ours/ccc/dataset_perturbed.py
+++ Ours/ccc/dataset_perturbed.py
@@ -16,11 +16,13 @@ def load(name, sigma, alpha):
         transform = T.Compose([T.NormalizeFeatures()]) #,T.ToDevice(device)
         dataset = Planetoid(root = '/scratch/midway3/ilgee/SelfGCon/Planetoid', name=name, transform=transform)
         temp = dataset[0]
-        noise = torch.normal(0, sigma, size=(temp.num_nodes, temp.num_features))
-        new_edge_index, _ = add_random_edge(temp.edge_index, alpha, force_undirected=True)
+        if sigma is not None:
+            noise = torch.normal(0, sigma, size=(temp.num_nodes, temp.num_features))
+            feat = temp.x + noise
+        if alpha is not None:
+            new_edge_index, _ = add_random_edge(temp.edge_index, alpha, force_undirected=True)
+            data.edge_index = new_edge_index
         data = deepcopy(temp)
-        data.edge_index = new_edge_index
-        feat = temp.x + noise
         data.x = feat
         train_idx = data.train_mask 
         val_idx = data.val_mask 
