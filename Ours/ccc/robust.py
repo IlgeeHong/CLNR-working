8d9a090b8316682575af4e8237d0b7d3a6cbbb38
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-01-09 19:38:05 -0600

    -

--- Ours/ccc/robust.py
+++ Ours/ccc/robust.py
@@ -26,7 +26,7 @@ from statistics import mean, stdev
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='Cora') #
 parser.add_argument('--n_experiments', type=int, default=2) #
-parser.add_argument('--n_layers', type=int, default=2) #3
+parser.add_argument('--n_layers', type=int, default=20) #3
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=5e-3)
 parser.add_argument('--wd2', type=float, default=1e-4)
@@ -38,7 +38,7 @@ parser.add_argument('--lambd', type=float, default=1e-3) # citeseer, computer 5e
 parser.add_argument('--batch', type=int, default=1024) #None
 # parser.add_argument('--sigma', type=float, default=1.0) #None
 parser.add_argument('--outlier', type=float, default=None) #None
-parser.add_argument('--alpha', type=float, default=None) #None
+parser.add_argument('--alpha', type=float, default=0.2) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
 parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/Robust")
 
@@ -48,7 +48,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # newly added
 results =[]
-for args.sigma in torch.arange(0,1.5,0.1): #'CLNR-unif','CLNR-align','bCLNR','nCLNR',
+for args.sigma in torch.arange(0,1.5,0.2): #'CLNR-unif','CLNR-align','bCLNR','nCLNR',
     for args.model in ['CLNR','dCLNR','GRACE','CCA-SSG']: #'CLNR-unif','CLNR-align','bCLNR','nCLNR',
     # for args.model in ['CLNR']:
         if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
