28c02927608fd8d11b04ee087a8370af6e4e5962
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-01-23 08:20:12 -0600

    -

--- Ours/ccc/robust.py
+++ Ours/ccc/robust.py
@@ -24,23 +24,23 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='ogbn-arxiv') #
-parser.add_argument('--n_experiments', type=int, default=3) #
-parser.add_argument('--n_layers', type=int, default=3) #3
+parser.add_argument('--dataset', type=str, default='PubMed') #
+parser.add_argument('--n_experiments', type=int, default=20) #
+parser.add_argument('--n_layers', type=int, default=2) #3
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=512) 
-parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2
+parser.add_argument('--fmr', type=float, default=0.3) #0.0 #0.2
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5
-parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer, ogbn-arxiv 5e-4
+parser.add_argument('--lambd', type=float, default=1e-3) # citeseer, computer, ogbn-arxiv 5e-4
 parser.add_argument('--batch', type=int, default=1024) #None
-parser.add_argument('--sigma', type=float, default=10.0) #None
+parser.add_argument('--sigma', type=float, default=None) #None
 parser.add_argument('--alpha', type=float, default=None) #None
 parser.add_argument('--outlier', type=bool, default=None) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
-parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/Robust")
+parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/Robust_no_out")
 
 args = parser.parse_args()
 file_path = os.getcwd() + args.result_file
@@ -53,12 +53,12 @@ results =[]
 # for args.model in ['CLNR','dCLNR','GRACE','GRACE_CCA','CCA-SSG','dCCA-SSG']:
 for args.model in ['CLNR','dCLNR','GRACE','CCA-SSG']:
     if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
-        args.epochs = 10000 # 10000
+        args.epochs = 600 # 10000
         args.lr1 = 1e-3 # 1e-2
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['GRACE']:
-        args.epochs = 10000
+        args.epochs = 1000
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
