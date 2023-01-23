0155db9d6e73b1e0092f4cf4b14e00e8be55a2f5
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-01-22 19:49:25 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -30,14 +30,14 @@ parser.add_argument('--n_layers', type=int, default=3) #3
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd2', type=float, default=1e-4)
-parser.add_argument('--hid_dim', type=int, default=512)
-parser.add_argument('--out_dim', type=int, default=512) 
+parser.add_argument('--hid_dim', type=int, default=1024)
+parser.add_argument('--out_dim', type=int, default=1024) 
 parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2 # 0.0
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5 # ogbn
 parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer, ogbn-arxiv 5e-4 
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
-parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/metric")
+parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/metric1")
 # parser.add_argument('--model', type=str, default='CLNR')
 # # parser.add_argument('--loss_type', type=str, default='ntxent') #None 
 # parser.add_argument('--epochs', type=int, default=50)
