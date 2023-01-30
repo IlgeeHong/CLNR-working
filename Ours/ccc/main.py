c6e8d6623743cc276006f0eebd55129500c17854
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-01-30 15:03:08 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -24,20 +24,20 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='ogbn-arxiv') #
-parser.add_argument('--n_experiments', type=int, default=5) #
-parser.add_argument('--n_layers', type=int, default=3) #3
+parser.add_argument('--dataset', type=str, default='Cora') #ogbn-arxiv
+parser.add_argument('--n_experiments', type=int, default=20) #
+parser.add_argument('--n_layers', type=int, default=2) #3
 parser.add_argument('--tau', type=float, default=0.5) 
-parser.add_argument('--lr2', type=float, default=1e-2)
+parser.add_argument('--lr2', type=float, default=5e-3)
 parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--hid_dim', type=int, default=1024)
 parser.add_argument('--out_dim', type=int, default=1024) 
-parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2 # 0.0
+parser.add_argument('--fmr', type=float, default=0.2) #0.0 #0.2 # 0.0
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5 # ogbn
-parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer, ogbn-arxiv 5e-4 
+parser.add_argument('--lambd', type=float, default=1e-3) # citeseer, computer, ogbn-arxiv 5e-4 
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
-parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/metric1")
+parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/Cora")
 # parser.add_argument('--model', type=str, default='CLNR')
 # # parser.add_argument('--loss_type', type=str, default='ntxent') #None 
 # parser.add_argument('--epochs', type=int, default=50)
@@ -53,32 +53,32 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 results =[]
 # for args.model in ['nCLNR','CLNR','dCLNR','GRACE','CCA-SSG']: #'CLNR-unif','CLNR-align','bCLNR',
 # for args.epochs in [50]:
-for args.model in ['dCCA-SSG']:#,'CLNR','GRACE','CCA-SSG','GRACE_CCA','dCCA-SSG']:
+for args.model in ['dCCA-SSG','gCCA-SSG','CCA-SSG','dCLNR','GRACE','CLNR']:#,'CLNR','GRACE','CCA-SSG','GRACE_CCA','dCCA-SSG']:
     if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
-        args.epochs = 10000
+        args.epochs = 50
         args.lr1 = 1e-3 # 1e-2
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['GRACE']:
-        args.epochs = 10000
-        args.lr1 = 1e-3
+        args.epochs = 400
+        args.lr1 = 5e-4
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
-    elif args.model in ['CCA-SSG']:
+    elif args.model in ['CCA-SSG','dCCA-SSG','gCCA-SSG']:
         args.epochs = 50
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'cca'
-    elif args.model in ['GRACE_CCA']:
-        args.epochs = 10000
-        args.lr1 = 1e-3
-        args.wd1 = 0.0
-        args.loss_type = 'ntxent_cca'
-    elif args.model in ['dCCA-SSG']:
-        args.epochs = 50
-        args.lr1 = 1e-3
-        args.wd1 = 0.0
-        args.loss_type = 'dcca'   
+    # elif args.model in ['GRACE_CCA']:
+    #     args.epochs = 10000
+    #     args.lr1 = 1e-3
+    #     args.wd1 = 0.0
+    #     args.loss_type = 'ntxent_cca'
+    # elif args.model in ['dCCA-SSG']:
+    #     args.epochs = 50
+    #     args.lr1 = 1e-3
+    #     args.wd1 = 0.0
+    #     args.loss_type = 'dcca'   
     # elif args.model in ['CLNR-unif']:
     #     args.epochs = 600
     #     args.lr1 = 1e-3
@@ -116,4 +116,4 @@ for args.model in ['dCCA-SSG']:#,'CLNR','GRACE','CCA-SSG','GRACE_CCA','dCCA-SSG'
     #results += [[args.model, args.dataset, args.epochs, args.n_layers, args.tau, args.lr1, args.lr2, args.wd1, args.wd2, args.out_dim, args.edr, args.fmr, eval_acc_mean, eval_acc_std,args.loss_type]]#
     results += [[args.model, args.dataset, args.epochs, args.out_dim, eval_acc_mean, eval_acc_std, Lu_mean, Lu_std, La_mean, La_std]]#
 res = pd.DataFrame(results, columns=['model', 'dataset', 'epochs', 'out_dim', 'acc_mean', 'acc_std', 'Lu_mean', 'Lu_std', 'La_mean', 'La_std'])#, 
-res.to_csv(file_path + "_" + args.model + "_" + str(args.batch) + "_" + str(args.out_dim) + "_" + str(args.hid_dim) + "_" + args.dataset +  ".csv", index=False) #str(args.epochs)args.model + "_" + 
+res.to_csv(file_path + "_" + str(args.batch) + "_" + str(args.out_dim) + "_" + str(args.hid_dim) + "_" + args.dataset +  ".csv", index=False) #str(args.epochs) + args.model + "_" + 
