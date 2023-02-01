4f0a8a1b1a3fbab81419ff18125305513fab90ba
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-01-31 21:58:16 -0600

    -

--- Ours/ccc/model2.py
+++ Ours/ccc/model2.py
@@ -1,10 +1,14 @@
 import torch
 import torch.nn as nn
+import numpy as np
 import torch.nn.functional as F
 import random
 from torch_geometric.nn import GCNConv
 from dbn import *
 from aug import *
+import pdb
+from torch_geometric.loader import NeighborLoader
+# from ogb.nodeproppred import Evaluator
 
 # CUDA support
 # if torch.cuda.is_available():
@@ -49,107 +53,186 @@ class GCN(nn.Module):
                 self.convs.append(GCNConv(hid_dim, hid_dim))
             self.convs.append(GCNConv(hid_dim, out_dim))
 
-    def forward(self, x, edge_index, p):
+    def forward(self, x, edge_index):
         for i in range(self.n_layers - 1):
-            x = F.relu(self.convs[i](x, edge_index))
-            x = F.dropout(x, p, training=self.training)
-            
+            x = F.relu(self.convs[i](x, edge_index)) #
         x = self.convs[-1](x, edge_index)
         return x
 
 class Model(nn.Module):
-    def __init__(self, in_dim, hid_dim, out_dim, n_layers, tau, use_mlp=False):
+    def __init__(self, in_dim, hid_dim, out_dim, n_layers, tau, lambd, device, model="CLNR", use_mlp=False):
         super().__init__()
         if not use_mlp:
             self.backbone = GCN(in_dim, hid_dim, out_dim, n_layers)
         else:
             self.backbone = MLP(in_dim, hid_dim, out_dim)
         self.tau = tau
+        self.lambd = lambd
+        self.model = model
+        self.device = device
+        self.fc1 = nn.Linear(out_dim, out_dim * 2)
+        self.fc2 = nn.Linear(out_dim * 2, out_dim)
+        self.bn = nn.BatchNorm1d(out_dim)
 
-    def get_embedding(self, data, p):
-        out = self.backbone(data.x, data.edge_index, p)
+    def get_embedding(self, data):
+        out = self.backbone(data.x, data.edge_index) 
         # No projection head here
         return out.detach()
 
-    def forward(self, data1, data2, p1, p2):
+    def forward(self, data1, data2):
         # Encode the graph
-        h1 = self.backbone(data1.x, data1.edge_index, p1)
-        h2 = self.backbone(data2.x, data2.edge_index, p2)
-        z1 = (h1 - h1.mean(0)) / h1.std(0)
-        z2 = (h2 - h2.mean(0)) / h2.std(0)
+        u = self.backbone(data1.x, data1.edge_index)
+        v = self.backbone(data2.x, data2.edge_index)
+        return u, v
+        
+    def projection(self, u, v):
+        if self.model == "GRACE":
+            u = F.elu(self.fc1(u))
+            v = F.elu(self.fc1(v))
+            z1 = self.fc2(u)
+            z2 = self.fc2(v)
+        elif self.model in ["CCA-SSG","CLNR","CLNR-unif","CLNR-align"]:
+            z1 = (u - u.mean(0)) / u.std(0)
+            z2 = (v - v.mean(0)) / v.std(0)
+        elif self.model in "dCCA-SSG":
+            dbn1 = DBN(device=u.device, num_features=u.shape[1], num_groups=1, dim=2, affine=False, momentum=1.)
+            dbn2 = DBN(device=v.device, num_features=v.shape[1], num_groups=1, dim=2, affine=False, momentum=1.)
+            z1 = dbn1(u)
+            z2 = dbn2(v)
+        elif self.model == "dCLNR":
+            dbn1 = DBN(device=u.device, num_features=u.shape[1], num_groups=1, dim=2, affine=False, momentum=1.)
+            dbn2 = DBN(device=v.device, num_features=v.shape[1], num_groups=1, dim=2, affine=False, momentum=1.)
+            z1 = dbn1(u)
+            z2 = dbn2(v)
+        elif self.model in ["dCCA-SSG","dCCA"]:
+            dbn1 = DBN(device=u.device, num_features=u.shape[1], num_groups=1, dim=2, affine=False, momentum=1.)
+            dbn2 = DBN(device=v.device, num_features=v.shape[1], num_groups=1, dim=2, affine=False, momentum=1.)
+            z1 = dbn1(u)
+            z2 = dbn2(v)
+        elif self.model == "gCCA-SSG":
+            u = F.elu(self.fc1(u))
+            v = F.elu(self.fc1(v))
+            z1 = self.fc2(u)
+            z2 = self.fc2(v)       
+        elif self.model == "bCLNR":
+            z1 = self.bn(u)
+            z2 = self.bn(v)
+        else:
+            z1 = u              
+            z2 = v
         return z1, z2
     
-    def sim(self, z1, z2):
+    def sim(self, z1, z2, indices):
         z1 = F.normalize(z1)
         z2 = F.normalize(z2)
-        return torch.mm(z1, z2.t())
+        return torch.mm(z1[indices,:].squeeze(), z2[indices,:].squeeze().T)
 
-    def semi_loss(self, z1, z2, indices):
+    def semi_loss(self, z1, z2, indices, loss_type='ntxent'):
         f = lambda x: torch.exp(x / self.tau)
-        if indices is not None:
-            z1 = z1[indices,:]
-            z2 = z2[indices,:]
-        refl_sim = f(self.sim(z1, z1))
-        between_sim = f(self.sim(z1, z2))
-        return -torch.log(between_sim.diag() / (refl_sim.sum(1) + between_sim.sum(1) - refl_sim.diag()))
+        if loss_type in ["ntxent", "ntxent_cca"]:
+            refl_sim = f(self.sim(z1, z1, indices))
+            between_sim = f(self.sim(z1, z2, indices))   
+            loss = -torch.log(between_sim.diag() / (refl_sim.sum(1) + between_sim.sum(1) - refl_sim.diag()))
+        return loss
 
-    def loss(self, z1, z2, k=None, mean = True):
+    def loss(self, u, v, k, loss_type='ntxent', mean = True):
         if k is not None:
-            N = z1.shape[0]
+            N = u.shape[0]
             indices = torch.LongTensor(random.sample(range(N), k))
         else:
             indices = None
-        l1 = self.semi_loss(z1, z2, indices)
-        l2 = self.semi_loss(z2, z1, indices)
-        ret = (l1 + l2) * 0.5
-        ret = ret.mean() if mean else ret.sum()
+        z1, z2 = self.projection(u, v)    
+        if loss_type == "ntxent":
+            l1 = self.semi_loss(z1, z2, indices, loss_type)
+            l2 = self.semi_loss(z2, z1, indices, loss_type)
+            ret = (l1 + l2) * 0.5
+            ret = ret.mean() if mean else ret.sum()
+        elif loss_type == 'cca':
+            N = z1.shape[0]
+            c = torch.mm(z1.T, z2)
+            c1 = torch.mm(z1.T, z1)
+            c2 = torch.mm(z2.T, z2)
+            c = c / N
+            c1 = c1 / N
+            c2 = c2 / N
+            loss_inv = - torch.diagonal(c).sum()
+            iden = torch.tensor(np.eye(c.shape[0])).to(self.device)
+            loss_dec1 = (iden - c1).pow(2).sum()
+            loss_dec2 = (iden - c2).pow(2).sum()
+            ret = loss_inv + self.lambd * (loss_dec1 + loss_dec2)
         return ret
 
 class ContrastiveLearning(nn.Module):
     def __init__(self, args, data, device):
         super().__init__()
+        self.dataset = args.dataset
         self.model = args.model
         self.epochs = args.epochs
-        self.p1 = args.p1
-        self.p2 = args.p2
+        self.fmr = args.fmr
+        self.edr = args.edr
+        self.lambd = args.lambd
         self.batch = args.batch
+        self.loss_type = args.loss_type
         self.data = data
         self.device = device
         self.num_class = int(self.data.y.max().item()) + 1 
-        self.model = Model(self.data.num_features, args.hid_dim, args.out_dim, args.n_layers, args.tau, use_mlp = args.mlp_use)
+        self.model = Model(self.data.num_features, args.hid_dim, args.out_dim, args.n_layers, args.tau, args.lambd, self.device, self.model, args.mlp_use)
         self.model = self.model.to(self.device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=args.lr1, weight_decay=args.wd1)
+
+        # if self.dataset == "ogbn-arxiv":
+        #     self.s = lambda epoch: epoch / 1000 if epoch < 1000 else ( 1 + np.cos((epoch-1000) * np.pi / (self.epochs - 1000))) * 0.5
+        #     self.scheduler = torch.optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda=self.s)
+        # if self.dataset in ['Swissroll','Moon','Circles']:
+        #     self.logreg = LogReg(args.out_dim, 1)
+        # else:
         self.logreg = LogReg(args.out_dim, self.num_class)
         self.logreg = self.logreg.to(self.device)
         self.opt = torch.optim.Adam(self.logreg.parameters(), lr=args.lr2, weight_decay=args.wd2)
 
     def train(self):
+        # loader = NeighborLoader(self.data, num_neighbors=[30] * 2, batch_size = self.batch, input_nodes=self.data.train_mask)
         for epoch in range(self.epochs):
             self.model.train()
+            # for batch in loader:
+                # print(batch.edge_index)
+                # A = batch.edge_index[0]
+                # print(A.unique().shape)
             self.optimizer.zero_grad()
-            new_data1 = self.data.to(self.device)
-            new_data2 = self.data.to(self.device)
-            z1, z2 = self.model(new_data1, new_data2, self.p1, self.p2)   
-            loss = self.model.loss(z1, z2, self.batch)
+            new_data1 = random_aug(self.data, self.fmr, self.edr) #batch
+            new_data2 = random_aug(self.data, self.fmr, self.edr)
+            new_data1 = new_data1.to(self.device)
+            new_data2 = new_data2.to(self.device)
+            u, v = self.model(new_data1, new_data2)
+            loss = self.model.loss(u, v, self.batch, self.loss_type)
             loss.backward()
             self.optimizer.step()
-            print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss))
+
 
     def LinearEvaluation(self, train_idx, val_idx, test_idx):
-        embeds = self.model.get_embedding(self.data.to(self.device),p=0.0)
+        
+        if self.dataset == "ogbn-arxiv":
+            self.model = self.model.cpu()
+            embeds = self.model.get_embedding(self.data)
+            embeds = embeds.to(self.device)
+        else:
+            embeds = self.model.get_embedding(self.data.to(self.device))
+            
         train_embs = embeds[train_idx]
         val_embs = embeds[val_idx]
         test_embs = embeds[test_idx]
 
-        label = self.data.y
+        if self.dataset == "ogbn-arxiv":
+            label = self.data.y.squeeze()
+        else:
+            label = self.data.y
         label = label.to(self.device)
 
         train_labels = label[train_idx]
         val_labels = label[val_idx]
         test_labels = label[test_idx]
- 
-        loss_fn = nn.CrossEntropyLoss()
 
+        loss_fn = nn.CrossEntropyLoss()       
         best_val_acc = 0
         eval_acc = 0
 
@@ -157,8 +240,8 @@ class ContrastiveLearning(nn.Module):
             self.logreg.train()
             self.opt.zero_grad()
             logits = self.logreg(train_embs)
-            preds = torch.argmax(logits, dim=1)
-            train_acc = torch.sum(preds == train_labels).float() / train_labels.shape[0]
+            # preds = torch.argmax(logits, dim=1)
+            # train_acc = torch.sum(preds == train_labels).float() / train_labels.shape[0]
             loss = loss_fn(logits, train_labels)
             loss.backward()
             self.opt.step()
@@ -178,7 +261,5 @@ class ContrastiveLearning(nn.Module):
                     best_val_acc = val_acc
                     if test_acc > eval_acc:
                         eval_acc = test_acc
-            print('Epoch:{}, train_acc:{:.4f}, val_acc:{:4f}, test_acc:{:4f}'.format(epoch, train_acc, val_acc, test_acc))
-        print('Linear evaluation accuracy:{:.4f}'.format(eval_acc))
-        return test_embs, test_labels, eval_acc
-    
+
+        return eval_acc
--- Ours/ccc/robust.py
+++ Ours/ccc/robust.py
@@ -11,7 +11,7 @@ import pandas as pd
 from torch_geometric.datasets import Planetoid, Coauthor, Amazon
 import torch_geometric.transforms as T
 
-from model import * 
+from model2 import * 
 from dataset_perturbed import *
 from statistics import mean, stdev
 
@@ -50,67 +50,38 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 results =[]
 # for args.sigma in [None, 0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5]:#torch.arange(0,1.5,0.2): #'CLNR-unif','CLNR-align','bCLNR','nCLNR',
 for args.alpha in [0.0, 0.2, 0.4, 0.6, 0.8]:#torch.arange(0,1.5,0.2): #'CLNR-unif','CLNR-align','bCLNR','nCLNR',
-# for args.model in ['CLNR','dCLNR','GRACE','CCA-SSG']: #'CLNR-unif','CLNR-align','bCLNR','nCLNR',
-# for args.model in ['CLNR','dCLNR','GRACE','GRACE_CCA','CCA-SSG','dCCA-SSG']:
-    for args.model in ['GRACE']: #,'GRACE','CCA-SSG'
-    # for args.model in ['GRACE']:
+# for args.model in ['dCCA-SSG','gCCA-SSG','CCA-SSG','dCLNR']:
+# for args.model in ['CCA-SSG']:
+    for args.model in ['GRACE']:
         if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
             args.epochs = 10000
             args.lr1 = 1e-3 # 1e-2
             args.wd1 = 0.0
             args.loss_type = 'ntxent'
-        elif args.model in ['GRACE']:
+        elif args.model in ['GRACE','gCCA-SSG']:
             args.epochs = 10000
             args.lr1 = 1e-3
             args.wd1 = 0.0
             args.loss_type = 'ntxent'
-        elif args.model in ['CCA-SSG']:
-            args.epochs = 100
+        elif args.model in ['CCA-SSG','dCCA-SSG']:
+            args.epochs = 50
             args.lr1 = 1e-3
             args.wd1 = 0.0
             args.loss_type = 'cca'
-        # elif args.model in ['GRACE_CCA']:
-        #     args.epochs = 1000
-        #     args.lr1 = 1e-3
-        #     args.wd1 = 0.0
-        #     args.loss_type = 'ntxent_cca'
-        # elif args.model in ['dCCA-SSG']:
-        #     args.epochs = 100
-        #     args.lr1 = 1e-3
-        #     args.wd1 = 0.0
-        #     args.loss_type = 'dcca'         
-        # elif args.model in ['CLNR-unif']:
-        #     args.epochs = 600
-        #     args.lr1 = 1e-3
-        #     args.wd1 = 0.0
-        #     args.loss_type = 'ntxent-uniform'
-        # elif args.model in ['CLNR-align','nCLNR-align']:
-        #     args.epochs = 200
-        #     args.lr1 = 1e-3
-        #     args.wd1 = 0.0
-        #     args.loss_type = 'ntxent-align'
 
         eval_acc_list = []
-        uniformity_list = []
-        alignment_list = [] 
         for exp in range(args.n_experiments):
             data, train_idx, val_idx, test_idx = load(args.dataset, args.sigma, args.alpha, args.outlier)
             model = ContrastiveLearning(args, data, device)
             model.train()
-            eval_acc, Lu, La = model.LinearEvaluation(train_idx, val_idx, test_idx) #
+            eval_acc = model.LinearEvaluation(train_idx, val_idx, test_idx) #
             eval_acc_list.append(eval_acc.item())
-            uniformity_list.append(Lu.item())
-            alignment_list.append(La.item())
             
         eval_acc_mean = round(mean(eval_acc_list),4)
         eval_acc_std = round(stdev(eval_acc_list),4)
-        Lu_mean = round(mean(uniformity_list),4)
-        Lu_std = round(stdev(uniformity_list),4)
-        La_mean = round(mean(alignment_list),4)
-        La_std = round(stdev(alignment_list),4)
 
         print('model: ' + args.model + ' done')
         #results += [[args.model, args.dataset, args.epochs, args.n_layers, args.tau, args.lr1, args.lr2, args.wd1, args.wd2, args.out_dim, args.edr, args.fmr, eval_acc_mean, eval_acc_std,args.loss_type]]#
-        results += [[args.model, args.dataset, args.epochs, args.alpha, args.outlier, eval_acc_mean, eval_acc_std, Lu_mean, Lu_std, La_mean, La_std]]#
-res = pd.DataFrame(results, columns=['model', 'dataset', 'epochs', 'alpha', 'outlier', 'acc_mean', 'acc_std', 'Lu_mean', 'Lu_std', 'La_mean', 'La_std'])#, 
+        results += [[args.model, args.dataset, args.epochs, args.alpha, args.outlier, eval_acc_mean, eval_acc_std]]#
+res = pd.DataFrame(results, columns=['model', 'dataset', 'epochs', 'alpha', 'outlier', 'acc_mean', 'acc_std'])#, 
 res.to_csv(file_path + "_" + args.model + "_" + str(args.batch) + "_" + str(args.out_dim) + "_" + str(args.hid_dim) + "_" + args.dataset + ".csv", index=False) #str(args.epochs)args.model + "_" + + "_" + str(args.sigma) + + args.model + "_" 
