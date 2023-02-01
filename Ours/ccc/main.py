2e2f41eba913ef006a74df0bcf9da70e0ccea17d
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-01-31 20:07:52 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -24,9 +24,9 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='Computers')
-parser.add_argument('--n_experiments', type=int, default=20)
-parser.add_argument('--n_layers', type=int, default=2)
+parser.add_argument('--dataset', type=str, default='ogbn-arxiv')
+parser.add_argument('--n_experiments', type=int, default=5)
+parser.add_argument('--n_layers', type=int, default=3)
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd2', type=float, default=1e-4)
@@ -37,7 +37,7 @@ parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5 # ogbn
 parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer, ogbn-arxiv 5e-4 
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
-parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/Final") #_test_epoch_lamb
+parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/Final1") #_test_epoch_lamb
 # parser.add_argument('--model', type=str, default='CLNR')
 # # parser.add_argument('--loss_type', type=str, default='ntxent') #None 
 # parser.add_argument('--epochs', type=int, default=50)
@@ -50,17 +50,17 @@ file_path = os.getcwd() + args.result_file
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 results =[]
-for args.model in ['dCCA-SSG','gCCA-SSG','CCA-SSG','dCLNR','GRACE','CLNR']:
-# for args.model in ['dCCA-SSG','gCCA-SSG','CCA-SSG','dCLNR']:
+# for args.model in ['dCCA-SSG','gCCA-SSG','CCA-SSG','dCLNR','GRACE','CLNR']:
+for args.model in ['dCCA-SSG','gCCA-SSG','CCA-SSG','dCLNR']:
 # for args.model in ['CCA-SSG']:
 # for args.model in ['GRACE','CLNR']:
     if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
-        args.epochs = 200
+        args.epochs = 10000
         args.lr1 = 1e-3 # 1e-2
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['GRACE','gCCA-SSG']:
-        args.epochs = 1000
+        args.epochs = 10000
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
