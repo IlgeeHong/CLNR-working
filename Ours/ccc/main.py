de73eb8b2b4e583086f5ff80e4a056ba9c92bcdd
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-01 18:36:18 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -24,25 +24,20 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='Computers')
+parser.add_argument('--dataset', type=str, default='CiteSeer')
 parser.add_argument('--n_experiments', type=int, default=20)
-parser.add_argument('--n_layers', type=int, default=2)
+parser.add_argument('--n_layers', type=int, default=1)
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=1e-2)
-parser.add_argument('--wd2', type=float, default=1e-4)
+parser.add_argument('--wd2', type=float, default=1e-2)
 parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=512) 
-parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2 # 0.0
+parser.add_argument('--fmr', type=float, default=0.2) #0.0 #0.2 # 0.0
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5 # ogbn
 parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer, ogbn-arxiv 5e-4 
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
 parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/Final_test") #_test_epoch_lamb
-# parser.add_argument('--model', type=str, default='CLNR')
-# # parser.add_argument('--loss_type', type=str, default='ntxent') #None 
-# parser.add_argument('--epochs', type=int, default=50)
-# parser.add_argument('--lr1', type=float, default=1e-3)
-# parser.add_argument('--wd1', type=float, default=0.0)
 
 args = parser.parse_args()
 
@@ -50,25 +45,26 @@ file_path = os.getcwd() + args.result_file
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 results =[]
-for args.model in ['dCCA-SSG','gCCA-SSG','CCA-SSG','dCLNR','GRACE','CLNR']:
+for args.model in ['dCLNR','GRACE','CLNR']:
+# ,'dCCA-SSG','gCCA-SSG','CCA-SSG'
 # for args.model in ['dCCA-SSG','gCCA-SSG','CCA-SSG','dCLNR']:
 # for args.model in ['CCA-SSG']:
 # for args.model in ['GRACE','CLNR']:
     if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
-        args.epochs = 200 # 10000
+        args.epochs = 50 # 10000
         args.lr1 = 1e-3 # 1e-2
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['GRACE','gCCA-SSG']:
-        args.epochs = 1000 # 10000
-        args.lr1 = 1e-3
+        args.epochs = 100 # 10000
+        args.lr1 = 5e-4
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
-    elif args.model in ['CCA-SSG','dCCA-SSG']:
-        args.epochs = 50
-        args.lr1 = 1e-3
-        args.wd1 = 0.0
-        args.loss_type = 'cca'                         
+    # elif args.model in ['CCA-SSG','dCCA-SSG']:
+    #     args.epochs = 50
+    #     args.lr1 = 1e-3
+    #     args.wd1 = 0.0
+    #     args.loss_type = 'cca'                         
 
     eval_acc_list = []
     uniformity_list = []
