eef106e89dc9b7069e465e076418c5d67db06fa0
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-01 21:00:29 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -24,16 +24,16 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='Photo')
+parser.add_argument('--dataset', type=str, default='Physics')
 parser.add_argument('--n_experiments', type=int, default=20)
 parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--tau', type=float, default=0.5) 
-parser.add_argument('--lr2', type=float, default=1e-2)
+parser.add_argument('--lr2', type=float, default=5e-3)
 parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=512) 
-parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2 # 0.0
-parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5 # ogbn
+parser.add_argument('--fmr', type=float, default=0.2) #0.0 #0.2 # 0.0
+parser.add_argument('--edr', type=float, default=0.4) #0.6 #0.5 # ogbn
 parser.add_argument('--lambd', type=float, default=1e-3) # citeseer, computer, ogbn-arxiv 5e-4 
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
@@ -51,7 +51,7 @@ for args.model in ['dCLNR','GRACE','CLNR']:
 # for args.model in ['CCA-SSG']:
 # for args.model in ['GRACE','CLNR']:
     if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
-        args.epochs = 100 # 10000
+        args.epochs = 50 # 10000
         args.lr1 = 1e-3 # 1e-2
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
