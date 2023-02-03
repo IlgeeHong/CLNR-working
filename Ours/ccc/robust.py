d160761f2caad950326a3574de0bcebb7425c1e5
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-03 16:28:05 -0600

    -

--- Ours/ccc/robust.py
+++ Ours/ccc/robust.py
@@ -24,15 +24,15 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='Computers') 
+parser.add_argument('--dataset', type=str, default='CiteSeer') 
 parser.add_argument('--n_experiments', type=int, default=20)
-parser.add_argument('--n_layers', type=int, default=2) 
+parser.add_argument('--n_layers', type=int, default=1) 
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=1e-2)
-parser.add_argument('--wd2', type=float, default=1e-4)
+parser.add_argument('--wd2', type=float, default=1e-2)
 parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=512) 
-parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2
+parser.add_argument('--fmr', type=float, default=0.2) #0.0 #0.2
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5
 parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer, ogbn-arxiv 5e-4
 parser.add_argument('--batch', type=int, default=1024) #None
@@ -52,13 +52,13 @@ for args.sigma in [0.0, 0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5]:
 # for args.alpha in [0.0, 0.2, 0.4, 0.6, 0.8]:
     for args.model in ['CCA-SSG','dCCA-SSG','gCCA-SSG','CLNR','dCLNR','GRACE']:
         if args.model in ['nCLNR','CLNR','bCLNR','dCLNR']:
-            args.epochs = 200
+            args.epochs = 50
             args.lr1 = 1e-3 # 1e-2
             args.wd1 = 0.0
             args.loss_type = 'ntxent'
         elif args.model in ['GRACE','gCCA-SSG']:
-            args.epochs = 1000
-            args.lr1 = 1e-3
+            args.epochs = 100
+            args.lr1 = 5e-4
             args.wd1 = 0.0
             args.loss_type = 'ntxent'
         elif args.model in ['CCA-SSG','dCCA-SSG']:
