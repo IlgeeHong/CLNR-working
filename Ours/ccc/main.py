66dae992bef0cf4c371e56f9dcfd44ad05a98d1b
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-06 17:30:41 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -49,7 +49,7 @@ results =[]
 # for args.model in ['CLNR','dCLNR','GRACE']:#'nCLNR',
 # ,'dCCA-SSG','gCCA-SSG','CCA-SSG'
 # for args.model in ['dCCA-SSG','gCCA-SSG','CCA-SSG','dCLNR']:
-args.model == 'CLNR'
+args.model = 'CLNR'
 # for args.model in ['CLNR']: #,'dCLNR'
 for args.epochs in [1000,2000,3000,4000,5000]: #,'dCLNR'
 # for args.model in ['GRACE','CLNR']:
