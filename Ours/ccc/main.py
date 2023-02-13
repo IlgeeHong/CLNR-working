6024d2dd8dd3fd5c121caec7dc497e7a08d385e3
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-12 20:05:28 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -24,20 +24,20 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='Cora')
+parser.add_argument('--dataset', type=str, default='CiteSeer')
 parser.add_argument('--n_experiments', type=int, default=20)
-parser.add_argument('--n_layers', type=int, default=2)
+parser.add_argument('--n_layers', type=int, default=1)
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=1e-2)
-parser.add_argument('--wd2', type=float, default=1e-4)
+parser.add_argument('--wd2', type=float, default=1e-2)
 parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=512) 
 parser.add_argument('--fmr', type=float, default=0.2) #0.0 #0.2 # 0.0
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5 # ogbn
-parser.add_argument('--lambd', type=float, default=1e-3) # citeseer, computer, ogbn-arxiv 5e-4 
+parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer, ogbn-arxiv 5e-4 
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
-parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/CORA") #Final_test1
+parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/CITESEER") #Final_test1
 
 args = parser.parse_args()
 
@@ -45,14 +45,14 @@ file_path = os.getcwd() + args.result_file
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 results =[]
-for args.model in ['nCLNR','CLNR','dCLNR','GRACE','GCLNR']: 
+for args.model in ['nCLNR','CLNR','GRACE','GCLNR']:  #'dCLNR',
     if args.model in ['nCLNR','bCLNR','dCLNR','CLNR','GCLNR']:
         args.epochs = 50 # 5000
         args.lr1 = 1e-3 # 1e-2
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['GRACE']:
-        args.epochs = 400 # 10000
+        args.epochs = 100 # 10000
         args.lr1 = 5e-4
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
