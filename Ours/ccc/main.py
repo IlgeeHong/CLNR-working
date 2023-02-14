76b3039ff76c0ec042c9acc464627a2d37862b2b
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-14 10:49:02 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -24,7 +24,7 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default="Computers")
+parser.add_argument('--dataset', type=str, default="Photo")
 parser.add_argument('--n_experiments', type=int, default=10)
 parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--tau', type=float, default=0.5) 
@@ -47,7 +47,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 results =[]
 for args.model in ['dCLNR']: # ,'nCLNR','CLNR','GRACE','GCLNR'
     if args.model in ['nCLNR','bCLNR','dCLNR','CLNR','GCLNR']:
-        args.epochs = 200
+        args.epochs = 100
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
