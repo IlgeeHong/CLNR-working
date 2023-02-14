d76eef84f015150f82b587c4f4d85c767f14dd74
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-13 19:04:20 -0600

    -

--- Ours/ccc/robust.py
+++ Ours/ccc/robust.py
@@ -51,8 +51,7 @@ results =[]
 # for args.sigma in [0.0, 0.01, 0.03, 0.05, 0.07, 0.09, 0.11, 0.13, 0.15]:
 # for args.sigma in [0.0, 0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5]:
 for args.alpha in [0.0, 0.2, 0.4, 0.6, 0.8]:
-    # for args.model in ['CCA-SSG','dCCA-SSG','gCCA-SSG','CLNR','dCLNR','GRACE']:
-    for args.model in ['nCLNR','CLNR','GRACE','GCLNR']:  #'dCLNR'
+    for args.model in ['nCLNR','CLNR','GRACE','GCLNR','dCLNR']:
         if args.model in ['nCLNR','bCLNR','dCLNR','CLNR','GCLNR']:
             args.epochs = 100
             args.lr1 = 1e-3 # 1e-2
