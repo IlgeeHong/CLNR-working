b77c18453aedd66a1240b430bae246f0340c4240
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-13 19:43:06 -0600

    -

--- Ours/ccc/robust.py
+++ Ours/ccc/robust.py
@@ -32,7 +32,7 @@ parser.add_argument('--lr2', type=float, default=5e-3)
 parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=512) 
-parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2
+parser.add_argument('--fmr', type=float, default=0.2) #0.0 #0.2
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5
 parser.add_argument('--lambd', type=float, default=1e-3) # citeseer, computer, ogbn-arxiv 5e-4
 parser.add_argument('--batch', type=int, default=1024) #None
