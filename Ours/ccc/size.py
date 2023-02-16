0cb6ab8aedf71041b4a266d461799f16f3d35633
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-16 11:22:55 -0600

    -

--- Ours/ccc/size.py
+++ Ours/ccc/size.py
@@ -24,15 +24,15 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default="Cora")
+parser.add_argument('--dataset', type=str, default="CiteSeer")
 parser.add_argument('--n_experiments', type=int, default=10)
-parser.add_argument('--n_layers', type=int, default=2)
+parser.add_argument('--n_layers', type=int, default=1)
 parser.add_argument('--tau', type=float, default=0.5) 
-parser.add_argument('--lr2', type=float, default=5e-3) # ogbn 5e-3 ???
-parser.add_argument('--wd2', type=float, default=1e-4) 
+parser.add_argument('--lr2', type=float, default=1e-2) # ogbn 5e-3 ???
+parser.add_argument('--wd2', type=float, default=1e-2) 
 parser.add_argument('--fmr', type=float, default=0.2) #0.0 #0.2 # 0.0 #
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5 # ogbn
-parser.add_argument('--lambd', type=float, default=1e-3) # citeseer, computer, ogbn-arxiv 5e-4 
+parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer, ogbn-arxiv 5e-4 
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--sigma', type=float, default=None) #None
 parser.add_argument('--alpha', type=float, default=None) #None
@@ -44,39 +44,39 @@ args = parser.parse_args()
 file_path = os.getcwd() + args.result_file
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-args.model = 'CCA-SSG' # 'CLNR','nCLNR','dCLNR','GRACE','GCLNR'
-results =[]
-for args.out_dim in [64,126,256,512]: # 
-    if args.model in ['nCLNR','bCLNR','dCLNR','CLNR','GCLNR']:
-        args.epochs = 50
-        args.hid_dim = args.out_dim        
-        args.lr1 = 1e-3
-        args.wd1 = 0.0
-        args.loss_type = 'ntxent'
-    elif args.model in ['GRACE']:
-        args.epochs = 400
-        args.hid_dim = args.out_dim
-        args.lr1 = 5e-4
-        args.wd1 = 0.0
-        args.loss_type = 'ntxent'
-    elif args.model in ['CCA-SSG']:
-        args.epochs = 50
-        args.hid_dim = args.out_dim
-        args.lr1 = 1e-3
-        args.wd1 = 0.0
-        args.loss_type = 'cca'                         
+for args.model in ['nCLNR','CLNR','GRACE','CCA-SSG']:
+    results =[]
+    for args.out_dim in [64,126,256,512]: 
+        if args.model in ['nCLNR','bCLNR','dCLNR','CLNR','GCLNR']:
+            args.epochs = 50
+            args.hid_dim = args.out_dim        
+            args.lr1 = 1e-3
+            args.wd1 = 0.0
+            args.loss_type = 'ntxent'
+        elif args.model in ['GRACE']:
+            args.epochs = 100
+            args.hid_dim = args.out_dim
+            args.lr1 = 5e-4
+            args.wd1 = 0.0
+            args.loss_type = 'ntxent'
+        elif args.model in ['CCA-SSG']:
+            args.epochs = 50
+            args.hid_dim = args.out_dim
+            args.lr1 = 1e-3
+            args.wd1 = 0.0
+            args.loss_type = 'cca'                         
 
-    eval_acc_list = []
-    for exp in range(args.n_experiments):
-        data, train_idx, val_idx, test_idx = load(args.dataset)
-        model = ContrastiveLearning(args, data, device)
-        model.train()
-        eval_acc = model.LinearEvaluation(train_idx, val_idx, test_idx) #
-        eval_acc_list.append(eval_acc.item())
-                    
-    eval_acc_mean = round(mean(eval_acc_list),4)
-    eval_acc_std = round(stdev(eval_acc_list),4)
-    print('model: ' + args.model + ' done')
-    results += [[args.model, args.dataset, args.epochs, args.out_dim, eval_acc_mean, eval_acc_std]]
+        eval_acc_list = []
+        for exp in range(args.n_experiments):
+            data, train_idx, val_idx, test_idx = load(args.dataset)
+            model = ContrastiveLearning(args, data, device)
+            model.train()
+            eval_acc = model.LinearEvaluation(train_idx, val_idx, test_idx) #
+            eval_acc_list.append(eval_acc.item())
+                        
+        eval_acc_mean = round(mean(eval_acc_list),4)
+        eval_acc_std = round(stdev(eval_acc_list),4)
+        print('model: ' + args.model + ' done')
+        results += [[args.model, args.dataset, args.epochs, args.out_dim, eval_acc_mean, eval_acc_std]]
 res = pd.DataFrame(results, columns=['model', 'dataset', 'epochs', 'out_dim', 'acc_mean', 'acc_std'])
-res.to_csv(file_path + "_" + args.model + "_" + str(args.batch) + "_" + args.dataset +  ".csv", index=False) 
+res.to_csv(file_path + "_" + str(args.batch) + "_" + args.dataset +  ".csv", index=False) #"_" + args.model + 
