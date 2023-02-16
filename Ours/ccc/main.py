5eba947ae6ca34204e3546e09e0c1379ea92599a
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-15 22:36:48 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -24,20 +24,20 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default="ogbn-arxiv")
-parser.add_argument('--n_experiments', type=int, default=1)
-parser.add_argument('--n_layers', type=int, default=3)
+parser.add_argument('--dataset', type=str, default="Physics")
+parser.add_argument('--n_experiments', type=int, default=5)
+parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=5e-3) # ogbn 5e-3 ???
-parser.add_argument('--wd2', type=float, default=1e-2)
+parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=512) 
-parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2 # 0.0 #
-parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5 # ogbn
+parser.add_argument('--fmr', type=float, default=0.2) #0.0 #0.2 # 0.0 #
+parser.add_argument('--edr', type=float, default=0.4) #0.6 #0.5 # ogbn
 parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer, ogbn-arxiv 5e-4 
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
-parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/Time") #Final_test1
+parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/PHYSICS") #Final_test1
 
 args = parser.parse_args()
 
@@ -45,14 +45,14 @@ file_path = os.getcwd() + args.result_file
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 results =[]
-for args.model in ['CLNR','GRACE']: # 'nCLNR','dCLNR','GRACE','GCLNR']
+for args.model in ['CLNR','nCLNR','dCLNR','GRACE','GCLNR']: # 
     if args.model in ['nCLNR','bCLNR','dCLNR','CLNR','GCLNR']:
-        args.epochs = 5000
-        args.lr1 = 1e-2
+        args.epochs = 50
+        args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['GRACE']:
-        args.epochs = 10000
+        args.epochs = 1000
         args.lr1 = 1e-3
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
@@ -69,27 +69,27 @@ for args.model in ['CLNR','GRACE']: # 'nCLNR','dCLNR','GRACE','GCLNR']
     for exp in range(args.n_experiments):
         data, train_idx, val_idx, test_idx = load(args.dataset)
         model = ContrastiveLearning(args, data, device)
-        recored_time = model.train()
+        model.train()
         eval_acc, Lu, La = model.LinearEvaluation(train_idx, val_idx, test_idx)
         eval_acc_list.append(eval_acc.item())
         uniformity_list.append(Lu.item())
         alignment_list.append(La.item())
         
-    eval_acc_mean = eval_acc_list
-    eval_acc_std = 0
-    Lu_mean = uniformity_list
-    Lu_std = 0
-    La_mean = alignment_list
-    La_std = 0
+    # eval_acc_mean = eval_acc_list
+    # eval_acc_std = 0
+    # Lu_mean = uniformity_list
+    # Lu_std = 0
+    # La_mean = alignment_list
+    # La_std = 0
 
-    # eval_acc_mean = round(mean(eval_acc_list),4)
-    # eval_acc_std = round(stdev(eval_acc_list),4)
-    # Lu_mean = round(mean(uniformity_list),4)
-    # Lu_std = round(stdev(uniformity_list),4)
-    # La_mean = round(mean(alignment_list),4)
-    # La_std = round(stdev(alignment_list),4)
+    eval_acc_mean = round(mean(eval_acc_list),4)
+    eval_acc_std = round(stdev(eval_acc_list),4)
+    Lu_mean = round(mean(uniformity_list),4)
+    Lu_std = round(stdev(uniformity_list),4)
+    La_mean = round(mean(alignment_list),4)
+    La_std = round(stdev(alignment_list),4)
 
     print('model: ' + args.model + ' done')
-    results += [[args.model, args.dataset, recored_time, args.epochs, args.out_dim, eval_acc_mean, eval_acc_std, Lu_mean, Lu_std, La_mean, La_std]]#
-res = pd.DataFrame(results, columns=['model', 'dataset', 'Time', 'epochs', 'out_dim', 'acc_mean', 'acc_std', 'Lu_mean', 'Lu_std', 'La_mean', 'La_std'])#, 
+    results += [[args.model, args.dataset, args.epochs, args.out_dim, eval_acc_mean, eval_acc_std, Lu_mean, Lu_std, La_mean, La_std]]#
+res = pd.DataFrame(results, columns=['model', 'dataset', 'epochs', 'out_dim', 'acc_mean', 'acc_std', 'Lu_mean', 'Lu_std', 'La_mean', 'La_std'])#, 
 res.to_csv(file_path + "_" + str(args.batch) + "_" + str(args.out_dim) + "_" + str(args.hid_dim) + "_" + args.dataset +  ".csv", index=False) #str(args.epochs) + args.model + "_" + 
