8721847deb8f9f446f283ed8090b9f2de033da35
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-17 15:06:15 -0600

    -

--- Ours/ccc/main.py
+++ Ours/ccc/main.py
@@ -24,20 +24,20 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default="Moon")
+parser.add_argument('--dataset', type=str, default="ogbn-arxiv")
 parser.add_argument('--n_experiments', type=int, default=2)
-parser.add_argument('--n_layers', type=int, default=2)
+parser.add_argument('--n_layers', type=int, default=3)
 parser.add_argument('--tau', type=float, default=0.5) 
 parser.add_argument('--lr2', type=float, default=1e-2) # ogbn 5e-3 ???
 parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=512) 
-parser.add_argument('--fmr', type=float, default=0.2) #0.0 #0.2 # 0.0 #
+parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2 # 0.0 #
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5 # ogbn
 parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer, ogbn-arxiv 5e-4 
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
-parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/FINAL") #Final_test1
+parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/TEST") #Final_test1
 
 args = parser.parse_args()
 
@@ -45,7 +45,7 @@ file_path = os.getcwd() + args.result_file
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 results =[]
-for args.model in ['CLNR']:# ['CLNR','nCLNR','dCLNR','GRACE','GCLNR']: # 
+for args.model in ['CCA-SSG']:# ['CLNR','nCLNR','dCLNR','GRACE','GCLNR']: # 
     if args.model in ['nCLNR','bCLNR','dCLNR','CLNR','GCLNR']:
         args.epochs = 100
         args.lr1 = 1e-3
@@ -57,8 +57,8 @@ for args.model in ['CLNR']:# ['CLNR','nCLNR','dCLNR','GRACE','GCLNR']: #
         args.wd1 = 0.0
         args.loss_type = 'ntxent'
     elif args.model in ['CCA-SSG']:
-        args.epochs = 50
-        args.lr1 = 1e-3
+        args.epochs = 5000
+        args.lr1 = 1e-2
         args.wd1 = 0.0
         args.loss_type = 'cca'                         
 
