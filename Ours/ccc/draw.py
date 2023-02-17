c277b66d0910f85f4802f98bfb00acbda8b54ca6
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-17 00:06:49 -0600

    -

--- Ours/ccc/draw.py
+++ Ours/ccc/draw.py
@@ -24,7 +24,7 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='PubMed') 
+parser.add_argument('--dataset', type=str, default='Computers') 
 parser.add_argument('--n_experiments', type=int, default=1)
 parser.add_argument('--n_layers', type=int, default=2) 
 parser.add_argument('--tau', type=float, default=0.5) 
@@ -32,9 +32,9 @@ parser.add_argument('--lr2', type=float, default=1e-2)
 parser.add_argument('--wd2', type=float, default=1e-4)
 parser.add_argument('--hid_dim', type=int, default=512)
 parser.add_argument('--out_dim', type=int, default=2) 
-parser.add_argument('--fmr', type=float, default=0.3) #0.0 #0.2
+parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5
-parser.add_argument('--lambd', type=float, default=1e-3) # citeseer, computer, ogbn-arxiv 5e-4
+parser.add_argument('--lambd', type=float, default=5e-4) # citeseer, computer, ogbn-arxiv 5e-4
 parser.add_argument('--batch', type=int, default=1024)
 parser.add_argument('--mlp_use', type=bool, default=False)
 parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/plotting")
@@ -45,7 +45,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 results =[]
 args.model = 'CLNR'
-args.epochs = 600
+args.epochs = 200
 args.lr1 = 1e-3
 args.wd1 = 0.0
 args.loss_type = 'ntxent'
