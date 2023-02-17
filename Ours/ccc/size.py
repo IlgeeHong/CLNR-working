6c4621d6860448245d2c43307e162c85932de008
IlgeeHong <77829046+IlgeeHong@users.noreply.github.com>
2023-02-16 18:37:58 -0600

    -

--- Ours/ccc/size.py
+++ Ours/ccc/size.py
@@ -24,20 +24,20 @@ from statistics import mean, stdev
 # physics : 1000 / 1e-3 / 0.0 /
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default="Cora")
+parser.add_argument('--dataset', type=str, default="Photo")
 parser.add_argument('--n_experiments', type=int, default=10)
 parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--tau', type=float, default=0.5) 
-parser.add_argument('--lr2', type=float, default=5e-3) # ogbn 5e-3 ???
+parser.add_argument('--lr2', type=float, default=1e-2) # ogbn 5e-3 ???
 parser.add_argument('--wd2', type=float, default=1e-4) 
-parser.add_argument('--fmr', type=float, default=0.2) #0.0 #0.2 # 0.0 #
+parser.add_argument('--fmr', type=float, default=0.0) #0.0 #0.2 # 0.0 #
 parser.add_argument('--edr', type=float, default=0.5) #0.6 #0.5 # ogbn
 parser.add_argument('--lambd', type=float, default=1e-3) # citeseer, computer, ogbn-arxiv 5e-4 
 parser.add_argument('--batch', type=int, default=1024) #None
 parser.add_argument('--sigma', type=float, default=None) #None
 parser.add_argument('--alpha', type=float, default=None) #None
 parser.add_argument('--mlp_use', type=bool, default=False)
-parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/FINAL") #Final_test1
+parser.add_argument('--result_file', type=str, default="/Ours/ccc/results/FINAL1") #Final_test1
 
 args = parser.parse_args()
 
@@ -45,18 +45,18 @@ file_path = os.getcwd() + args.result_file
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 results =[]
-for args.model in ['nCLNR','CLNR','GRACE','CCA-SSG']:
+for args.model in ['nCLNR','CLNR']: #,'GRACE','CCA-SSG'
     for args.out_dim in [64,126,256,512]: 
         if args.model in ['nCLNR','bCLNR','dCLNR','CLNR','GCLNR']:
-            args.epochs = 50
+            args.epochs = 100
             args.hid_dim = args.out_dim        
             args.lr1 = 1e-3
             args.wd1 = 0.0
             args.loss_type = 'ntxent'
         elif args.model in ['GRACE']:
-            args.epochs = 400
+            args.epochs = 1000
             args.hid_dim = args.out_dim
-            args.lr1 = 5e-4
+            args.lr1 = 1e-3
             args.wd1 = 0.0
             args.loss_type = 'ntxent'
         elif args.model in ['CCA-SSG']:
